03e8e71faf9f81fc598dce86d157277d7f6d8684
pochun-chiu <pochun.chiu@instill.tech>
2022-03-30 21:31:27 +0800

    fix: rollback InputLabel to solve children not defined issue

--- src/components/ui/InputLabel/InputLabel.tsx
+++ src/components/ui/InputLabel/InputLabel.tsx
@@ -15,31 +15,27 @@ export interface InputLabelProps {
   answered: boolean;
 }
 
-const InputLabel: FC<InputLabelProps> = ({
-  htmlFor,
-  required,
-  focus,
-  answered,
-  children,
-}) => {
-  const activate = focus ? true : answered ? true : false;
+const InputLabel: FC<InputLabelProps> = memo(
+  ({ htmlFor, required, focus, answered, children }) => {
+    const activate = focus ? true : answered ? true : false;
 
-  return (
-    <label
-      className={cn(
-        "absolute transform-gpu instill-text-body origin-top-left left-0 top-0 text-instillGray50",
-        activate
-          ? "top-1/2 translate-x-5 -translate-y-full"
-          : "top-1/2 translate-x-5 -translate-y-1/2"
-      )}
-      htmlFor={htmlFor}
-    >
-      {children}
-      {required ? <span className="ml-1">*</span> : null}
-    </label>
-  );
-};
+    return (
+      <label
+        className={cn(
+          "absolute transform-gpu instill-text-body origin-top-left left-0 top-0 text-instillGray50",
+          activate
+            ? "top-1/2 translate-x-5 -translate-y-full"
+            : "top-1/2 translate-x-5 -translate-y-1/2"
+        )}
+        htmlFor={htmlFor}
+      >
+        {children}
+        {required ? <span className="ml-1">*</span> : null}
+      </label>
+    );
+  }
+);
 
-export default memo(InputLabel);
+export default InputLabel;
 
 InputLabel.displayName = "InputLabel";
