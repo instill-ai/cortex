a918263bea43b43e21851754dc067486a7506f38
pochun-chiu <pochun.chiu@instill.tech>
2022-03-30 15:25:37 +0800

    fix: fix StoryGroup style issue

--- src/utils/StoryGroup.tsx
+++ src/utils/StoryGroup.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
 
 export const StoryGroup: FC = ({ children }) => {
-  return <div className="grid grid-cols-1 gap-y-4">{children}</div>;
+  return <div className="flex flex-col">{children}</div>;
 };
