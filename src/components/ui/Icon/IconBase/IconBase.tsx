f9c7b0980f9d8b8e5e2aaf4f941aebba74245f37
pochun-chiu <pochun.chiu@instill.tech>
2022-03-31 15:10:22 +0800

    feat: add IconBase

---
+++ src/components/ui/Icon/IconBase/IconBase.tsx
@@ -0,0 +1,38 @@
+import { FC } from "react";
+import cn from "clsx";
+
+export interface IconBaseProps {
+  /** The viewbox of target icon
+   * - e.g. "0 0 32 32"
+   */
+  viewBox: string;
+
+  /** TailwindCSS format - The width of icon. */
+  width: string;
+
+  /** TailwindCSS format - The height of icon. */
+  height: string;
+
+  /** TailwindCSS format - The color of icon. */
+  color: string;
+}
+
+const IconBase: FC<IconBaseProps> = ({
+  viewBox,
+  children,
+  width,
+  height,
+  color,
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={viewBox}
+      className={cn("fill-current", width, height, color)}
+    >
+      {children}
+    </svg>
+  );
+};
+
+export default IconBase;
