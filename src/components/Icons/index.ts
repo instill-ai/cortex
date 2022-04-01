96eb89113f483b51fb24255f0ee1a6289d3a4b76
pochun-chiu <pochun.chiu@instill.tech>
2022-04-01 15:31:13 +0800

    feat: export icon props type

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,4 +1,6 @@
 import EyeIcon from "./EyeIcon";
 import EyeSlashIcon from "./EyeSlashIcon";
+import type { EyeIconProps } from "./EyeIcon";
+import type { EyeSlashIconProps } from "./EyeSlashIcon";
 
-export { EyeIcon, EyeSlashIcon };
+export { EyeIcon, EyeIconProps, EyeSlashIcon, EyeSlashIconProps };
