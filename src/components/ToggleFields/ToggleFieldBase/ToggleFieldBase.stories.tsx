c5817158fc740aff1fc2ed4a50a2331e582da4ba
pochun-chiu <pochun.chiu@instill.tech>
2022-04-06 20:09:46 +0800

    feat: add ToggleFieldBase and its story

---
+++ src/components/ToggleFields/ToggleFieldBase/ToggleFieldBase.stories.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { ToggleFieldBase } from "./ToggleFieldBase";
+
+export default {
+  title: "Components/Base/Input/ToggleFieldBase",
+  component: ToggleFieldBase,
+} as ComponentMeta<typeof ToggleFieldBase>;
+
+const Template: ComponentStory<typeof ToggleFieldBase> = (args) => (
+  <ToggleFieldBase {...args} />
+);
+export const Playground: ComponentStory<typeof ToggleFieldBase> = Template.bind(
+  {}
+);
+
+export const Default: ComponentStory<typeof ToggleFieldBase> = () => {
+  const [text, setText] = useState<string>("");
+
+  const onChangeInput = (inputValue: string) => {
+    setText(inputValue);
+  };
+
+  return (
+    <ToggleFieldBase
+      id="toggle-field-base-playground"
+      disabled={false}
+      disabledBgColor=""
+      readOnly={false}
+      readOnlyBgColor=""
+      bgColor=""
+      onChangeInput={onChangeInput}
+      required={true}
+      labelName="toggle-field-base-playground"
+      focusHighlight={true}
+      borderRadius=""
+      inputLabelType="normal"
+    />
+  );
+};
---
+++ src/components/ToggleFields/ToggleFieldBase/ToggleFieldBase.tsx
@@ -0,0 +1,69 @@
+import { FC, useState } from "react";
+import cn from "clsx";
+import { BasicInputFieldAttributes } from "../../../types/general";
+import InputLabel from "../../InputLabel";
+
+export type ToggleButtonBaseProps = Omit<
+    BasicInputFieldAttributes,
+    | "valid"
+    | "placeholder"
+    | "fontSize"
+    | "lineHeight"
+    | "fontWeight"
+    | "textColor"
+    | "inputWidth"
+    | "inputHeight"
+    | "autoComplete"
+  >
+
+export const ToggleFieldBase: FC<ToggleButtonBaseProps> = ({
+  id,
+  disabled,
+  disabledBgColor,
+  readOnly,
+  readOnlyBgColor,
+  focusHighlight,
+  required,
+  onChangeInput,
+  labelName,
+}) => {
+  const [answered, setAnswered] = useState(false);
+  return (
+    <label
+      className={cn("flex flex-col cursor-pointer gap-y-2.5", focusHighlight)}
+      htmlFor={id}
+    >
+      <InputLabel
+        type="normal"
+        answered={answered}
+        required={required}
+        htmlFor={id}
+        fontStyle="font-normal text-sm leading-[18.2px]"
+        activateStyle="top-0 translate-y-3"
+        deActivateStyle="top-0 translate-y-[26px]"
+      >
+        {labelName}
+      </InputLabel>
+      <div className="flex flex-col relative">
+        <input
+          id={id}
+          className={cn(
+            "peer appearance-none cursor-pointer w-[90px] h-10 border border-intstillGrey15 checked:border-instillBlue30",
+            focusHighlight
+              ? "instill-input-highlight"
+              : "instill-input-no-highlight"
+          )}
+          type="checkbox"
+          role="switch"
+          onChange={(event) => {
+            onChangeInput(event.target.value);
+            if (!answered) {
+              setAnswered(true);
+            }
+          }}
+        />
+        <div className="absolute left-[5px] top-[5px] bg-instillGray30 w-[30px] h-[30px] origin-top-left transition peer-checked:translate-x-[50px] peer-checked:bg-instillBlue30" />
+      </div>
+    </label>
+  );
+};
