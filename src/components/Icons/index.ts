e0e03b345f1f8a32ce73f7b1abdd1f2d0d7d591a
pochun-chiu <pochun.chiu@instill.tech>
2022-04-07 15:35:00 +0800

    feat: export IdIcon

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,6 +1,15 @@
 import EyeIcon from "./EyeIcon";
-import EyeSlashIcon from "./EyeSlashIcon";
 import type { EyeIconProps } from "./EyeIcon";
+import EyeSlashIcon from "./EyeSlashIcon";
 import type { EyeSlashIconProps } from "./EyeSlashIcon";
+import IdIcon from "./IdIcon/IdIcon";
+import type { IdIconProps } from "./IdIcon";
 
-export { EyeIcon, EyeIconProps, EyeSlashIcon, EyeSlashIconProps };
+export {
+  EyeIcon,
+  EyeIconProps,
+  EyeSlashIcon,
+  EyeSlashIconProps,
+  IdIcon,
+  IdIconProps,
+};
