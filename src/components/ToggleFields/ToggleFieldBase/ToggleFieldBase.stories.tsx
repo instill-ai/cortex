9b079c3994bdc2ff0162858ee2acae40dc5cb2a0
pochun-chiu <pochun.chiu@instill.tech>
2022-04-13 14:25:18 +0800

    feat: rewrite ToggleFieldBase to make it work correctly

--- src/components/ToggleFields/ToggleFieldBase/ToggleFieldBase.stories.tsx
+++ src/components/ToggleFields/ToggleFieldBase/ToggleFieldBase.stories.tsx
@@ -11,22 +11,54 @@ const Template: ComponentStory<typeof ToggleFieldBase> = (args) => (
   <ToggleFieldBase
     {...args}
     dotColor="bg-instillGray30"
-    checkedDotColor="peer-checked:bg-instillBlue30"
+    checkedDotColor="peer-checked:bg-instillBlue50"
   />
 );
 export const Playground: ComponentStory<typeof ToggleFieldBase> = Template.bind(
   {}
 );
 
-export const Default: ComponentStory<typeof ToggleFieldBase> = () => {
-  const [text, setText] = useState<string>("");
+Playground.args = {
+  id: "toggle-field-base-playground",
+  defaultChecked: false,
+  disabled: false,
+  readOnly: false,
+  required: true,
+  labelName: "toggle-field-base-playground",
+  focusHighlight: true,
+  borderRadius: "",
+  dotColor: "bg-instillGray30",
+  checkedInputBorderColor: "checked:border-instillBlue50",
+  checkedDotColor: "peer-checked:bg-instillBlue50",
+  disabledDotColor: "disabled:bg-instillGray20",
+  disabledCheckedDotColor: "disabled:peer-checked:bg-[#8DF5FF]",
+  disabledCursor: "disabled:cursor-not-allowed",
+  disabledInputBgColor: "disabled:bg-white",
+  disabledInputBorderColor: "disabled:border-instillGray20",
+  disabledInputBorderStyle: "disabled:border-dashed",
+  disabledInputBorderWidth: "disabled:border",
+  disabledCheckedInputBorderColor: "disabled:checked:border-instillGray20",
+  readOnlyCursor: "read-only:cursor-auto",
+  readOnlyInputBgColor: "read-only:bg-white",
+  readOnlyInputBorderColor: "read-only:border-instillGray20",
+  readOnlyInputBorderStyle: "read-only:border-solid",
+  readOnlyInputBorderWidth: "read-only:border",
+  readOnlyCheckedInputBorderColor: "read-only:checked:border-[#8DF5FF]",
+  readOnlyCheckedDotColor: "read-only:peer-checked:bg-[#8DF5FF]",
+  readOnlyDotColor: "read-only:bg-instillGray20",
+};
 
-  const onChangeInput = (inputValue: string) => {
-    setText(inputValue);
+export const Default: ComponentStory<typeof ToggleFieldBase> = () => {
+  const [checked, setChecked] = useState(false);
+
+  const onChangeInput = (inputValue: any) => {
+    console.log(inputValue);
+    setChecked(inputValue);
   };
 
   return (
     <ToggleFieldBase
+      defaultChecked={true}
       id="toggle-field-base-playground"
       disabled={false}
       readOnly={false}
@@ -36,7 +68,24 @@ export const Default: ComponentStory<typeof ToggleFieldBase> = () => {
       focusHighlight={true}
       borderRadius=""
       dotColor="bg-instillGray30"
-      checkedDotColor="peer-checked:bg-instillBlue30"
+      checkedInputBorderColor="checked:border-instillBlue50"
+      checkedDotColor="peer-checked:bg-instillBlue50"
+      disabledDotColor="disabled:bg-instillGray20"
+      disabledCheckedDotColor="disabled:peer-checked:bg-[#8DF5FF]"
+      disabledCursor="disabled:cursor-not-allowed"
+      disabledInputBgColor="disabled:bg-white"
+      disabledInputBorderColor="disabled:border-instillGray20"
+      disabledInputBorderStyle="disabled:border-dashed"
+      disabledInputBorderWidth="disabled:border"
+      disabledCheckedInputBorderColor="disabled:checked:border-instillGray20"
+      readOnlyCursor="read-only:cursor-auto"
+      readOnlyInputBgColor="read-only:bg-white"
+      readOnlyInputBorderColor="read-only:border-instillGray20"
+      readOnlyInputBorderStyle="read-only:border-solid"
+      readOnlyInputBorderWidth="read-only:border"
+      readOnlyCheckedInputBorderColor="read-only:checked:border-[#8DF5FF]"
+      readOnlyCheckedDotColor="read-only:peer-checked:bg-[#8DF5FF]"
+      readOnlyDotColor="readonly:bg-instillGray20"
     />
   );
 };
--- src/components/ToggleFields/ToggleFieldBase/ToggleFieldBase.tsx
+++ src/components/ToggleFields/ToggleFieldBase/ToggleFieldBase.tsx
@@ -14,25 +14,75 @@ export type ToggleFieldBaseProps = Omit<
   | "inputWidth"
   | "inputHeight"
   | "autoComplete"
-  | "disabledBgColor"
-  | "readOnlyBgColor"
   | "inputLabelType"
   | "bgColor"
+  | "disabledInputTextColor"
+  | "readOnlyInputTextColor"
 > & {
+  /** The initial checked state of this toggle field */
+  defaultChecked: boolean;
+
   /** TailwindCSS format - Toggle center's dot color
    * - Please use background-color, e.g. bg-blqck
    */
+
   dotColor: string;
 
   /** TailwindCSS format - Toggle center's dot color when checked
-   * - Please use tailwindCSS peer-checked: persudo class, e.g. peer-checked:bg-black
+   * - We have peer on input, please use tailwindCSS persudo class "peer-checked:"
+   * - e.g. peer-checked:bg-black
    */
   checkedDotColor: string;
+
+  /** TailwindCSS format - Toggle border color when checked
+   * - Please use tailwindCSS persudo class "checked:"
+   * - e.g. checked:border-black
+   */
+  checkedInputBorderColor: string;
+
+  /** TailwindCSS format - Toggle input border color when checked and disabled
+   * - Please use tailwindCSS persudo class "disabled:checked:"
+   * - e.g. disabled:checked:border-black
+   */
+  disabledCheckedInputBorderColor: string;
+
+  /** TailwindCSS format - Toggle center's dot color when disabled
+   * - Please use tailwindCSS persudo class "disabled:"
+   * - e.g. disabled:bg-black
+   */
+  disabledDotColor: string;
+
+  /** TailwindCSS format - Toggle center's dot color when checked and disabled
+   * - We have peer on input, please use tailwindCSS persudo class "disabled:peer-checked:"
+   * - e.g. disabled:peer-checked:bg-black
+   */
+  disabledCheckedDotColor: string;
+
+  /** TailwindCSS format - Toggle center's dot color when read-only
+   * - We have peer on input, please use tailwindCSS persudo class "read-only:"
+   * - e.g. read-only:bg-black
+   */
+  readOnlyDotColor: string;
+
+  /** TailwindCSS format - Toggle center's dot color when checked and read-only
+   * - We have peer on input, please use tailwindCSS persudo class "read-only:peer-checked:"
+   * - e.g. read-only:peer-checked:bg-black
+   */
+
+  readOnlyCheckedDotColor: string;
+
+  /** TailwindCSS format - Toggle input border color when checked and read-only
+   * - Please use tailwindCSS persudo class "read-only:checked:"
+   * - e.g. read-only:checked:border-black
+   */
+
+  readOnlyCheckedInputBorderColor: string;
 };
 
 const ToggleFieldBase: FC<ToggleFieldBaseProps> = ({
   borderRadius,
   id,
+  defaultChecked,
   disabled,
   readOnly,
   focusHighlight,
@@ -41,6 +91,23 @@ const ToggleFieldBase: FC<ToggleFieldBaseProps> = ({
   labelName,
   dotColor,
   checkedDotColor,
+  checkedInputBorderColor,
+  disabledCursor,
+  disabledInputBgColor,
+  disabledInputBorderColor,
+  disabledInputBorderStyle,
+  disabledInputBorderWidth,
+  disabledCheckedInputBorderColor,
+  disabledDotColor,
+  disabledCheckedDotColor,
+  readOnlyCursor,
+  readOnlyInputBgColor,
+  readOnlyInputBorderColor,
+  readOnlyInputBorderStyle,
+  readOnlyInputBorderWidth,
+  readOnlyDotColor,
+  readOnlyCheckedInputBorderColor,
+  readOnlyCheckedDotColor,
 }) => {
   const [answered, setAnswered] = useState(false);
   return (
@@ -61,31 +128,72 @@ const ToggleFieldBase: FC<ToggleFieldBaseProps> = ({
           id={id}
           aria-label={`${id}-label`}
           className={cn(
-            "peer appearance-none cursor-pointer w-[90px] h-10 border border-intstillGrey15",
+            "peer appearance-none w-[90px] h-10 border border-intstillGrey15",
             disabled
               ? "border border-instillGray15"
               : readOnly
               ? "border border-instillGray15"
               : focusHighlight
-              ? "instill-input-highlight checked:border-instillBlue30"
-              : "instill-input-no-highlight border border-instillGray15 checked:border-instillBlue30",
-            borderRadius
+              ? "instill-input-highlight checked:border-instillBlue50"
+              : "instill-input-no-highlight border border-instillGray15 checked:border-instillBlue50",
+            borderRadius,
+            checkedInputBorderColor,
+            disabled
+              ? cn(
+                  disabledCursor,
+                  disabledInputBgColor,
+                  disabledInputBorderColor,
+                  disabledInputBorderStyle,
+                  disabledInputBorderWidth,
+                  disabledCheckedInputBorderColor
+                )
+              : readOnly
+              ? cn(
+                  readOnlyCursor,
+                  readOnlyInputBgColor,
+                  readOnlyInputBorderColor,
+                  readOnlyInputBorderStyle,
+                  readOnlyInputBorderWidth,
+                  readOnlyCheckedInputBorderColor
+                )
+              : "cursor-pointer"
           )}
           type="checkbox"
           role="switch"
           disabled={disabled}
           readOnly={readOnly}
           onChange={(event) => {
-            onChangeInput(event.target.value);
+            if (readOnly) {
+              return;
+            }
+
+            onChangeInput(event.target.checked);
+
             if (!answered) {
               setAnswered(true);
             }
           }}
+          onClick={(event) => {
+            if (readOnly) {
+              event.stopPropagation();
+              event.preventDefault();
+              return false;
+            }
+          }}
+          defaultChecked={defaultChecked}
         />
         <div
           className={cn(
-            "absolute cursor-pointer left-[5px] top-[5px] w-[30px] h-[30px] origin-top-left transition peer-checked:translate-x-[50px]",
-            disabled ? "" : readOnly ? "" : checkedDotColor,
+            "absolute left-[5px] top-[5px] w-[30px] h-[30px] origin-top-left transition peer-checked:translate-x-[50px]",
+            disabled
+              ? cn(
+                  disabledDotColor,
+                  disabledCheckedDotColor,
+                  "cursor-not-allowed"
+                )
+              : readOnly
+              ? cn(readOnlyDotColor, readOnlyCheckedDotColor, "cursor-auto")
+              : cn(checkedDotColor, "cursor-pointer"),
             dotColor,
             borderRadius
           )}
