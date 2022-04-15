dd74fc4d565b3343da5ce09d615132dd8e959bcc
pochun-chiu <pochun.chiu@instill.tech>
2022-04-15 19:45:19 +0800

    feat: adapt new border style at BasicTextField

--- src/components/TextFields/BasicTextField/BasicTextField.stories.tsx
+++ src/components/TextFields/BasicTextField/BasicTextField.stories.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import BasicTextField from "./BasicTextField";
 
@@ -16,28 +15,13 @@ export const Playground: ComponentStory<typeof BasicTextField> = Template.bind(
 );
 
 Playground.args = {
+  error: null,
+  id: "basic-text-field-playground",
   labelName: "basic-text-field-playground",
-};
-
-export const Default: ComponentStory<typeof BasicTextField> = () => {
-  const [text, setText] = useState<string>("");
-
-  const onChangeInput = (inputValue: string) => {
-    setText(inputValue);
-  };
-
-  return (
-    <BasicTextField
-      error={null}
-      onChangeInput={onChangeInput}
-      id="basic-text-field-playground"
-      labelName="basic-text-field-playground"
-      required={true}
-      autoComplete="off"
-      type="text"
-      disabled={false}
-      placeholder="hello!"
-      readOnly={false}
-    />
-  );
+  required: true,
+  autoComplete: "off",
+  type: "text",
+  disabled: false,
+  placeholder: "hello!",
+  readOnly: false,
 };
