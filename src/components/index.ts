e7b1243842bf424b10e254cd2d488de9cb4138cb
pochun-chiu <pochun.chiu@instill.tech>
2022-04-19 13:58:00 +0800

    feat: export necessary components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,49 @@
 export * from "./Icons";
 
-export { BasicTextField, ProtectedBasicTextField } from "./TextFields";
-export type {
+import { AutoCompleteWithIcon } from "./AutoCompletes";
+import type { AutoCompleteWithIconProps } from "./AutoCompletes";
+
+import { BasicInputLabel, TextAreaInputLabel } from "./InputLabels";
+import type {
+  BasicInputLabelProps,
+  TextAreaInputLabelProps,
+} from "./InputLabels";
+
+import Logo from "./Logo";
+import type { LogoProps } from "./Logo";
+
+import { BasicTextArea } from "./TextAreas";
+import type { BasicTextAreaProps } from "./TextAreas";
+
+import { BasicTextField, ProtectedBasicTextField } from "./TextFields";
+import type {
   BasicTextFieldProps,
   ProtectedBasicTextFieldProps,
 } from "./TextFields";
+
+import { BasicToggleField } from "./ToggleFields";
+import type { BasicToggleFieldProps } from "./ToggleFields";
+
+import { BasicUploadFileField } from "./UploadFileFields";
+import type { BasicUploadFileFieldProps } from "./UploadFileFields";
+
+export {
+  AutoCompleteWithIcon,
+  AutoCompleteWithIconProps,
+  BasicInputLabel,
+  BasicInputLabelProps,
+  TextAreaInputLabel,
+  TextAreaInputLabelProps,
+  Logo,
+  LogoProps,
+  BasicTextArea,
+  BasicTextAreaProps,
+  BasicTextField,
+  BasicTextFieldProps,
+  ProtectedBasicTextField,
+  ProtectedBasicTextFieldProps,
+  BasicToggleField,
+  BasicToggleFieldProps,
+  BasicUploadFileField,
+  BasicUploadFileFieldProps,
+};
