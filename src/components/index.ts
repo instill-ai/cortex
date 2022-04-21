84efb14ebb493ad092f3e26b0b5eed038b100e47
pochun-chiu <pochun.chiu@instill.tech>
2022-04-21 15:44:28 +0800

    feat: export Progress

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,34 +1,55 @@
-export * from "./Icons";
-
+// AutoCompletes
 import { AutoCompleteWithIcon } from "./AutoCompletes";
 import type { AutoCompleteWithIconProps } from "./AutoCompletes";
 
+// Icons
+export * from "./Icons";
+
+// InputDescriptions
+import { BasicInputDescription } from "./InputDescriptions";
+import type { BasicInputDescriptionProps } from "./InputDescriptions";
+
+// InputLabels
 import { BasicInputLabel, TextAreaInputLabel } from "./InputLabels";
 import type {
   BasicInputLabelProps,
   TextAreaInputLabelProps,
 } from "./InputLabels";
 
+// Logos
 import Logo from "./Logo";
 import type { LogoProps } from "./Logo";
 
+// Progress
+import {
+  WhiteBgSquareProgress,
+  DarkBgSquareProgress,
+  NoBgSquareProgress,
+} from "./Progress";
+import type {
+  WhiteBgSquareProgressProps,
+  DarkBgSquareProgressProps,
+  NoBgSquareProgressProps,
+} from "./Progress";
+
+// TextAreas
 import { BasicTextArea } from "./TextAreas";
 import type { BasicTextAreaProps } from "./TextAreas";
 
+// TextFields
 import { BasicTextField, ProtectedBasicTextField } from "./TextFields";
 import type {
   BasicTextFieldProps,
   ProtectedBasicTextFieldProps,
 } from "./TextFields";
 
+// ToggleFields
 import { BasicToggleField } from "./ToggleFields";
 import type { BasicToggleFieldProps } from "./ToggleFields";
 
+// UploadFileFields
 import { BasicUploadFileField } from "./UploadFileFields";
 import type { BasicUploadFileFieldProps } from "./UploadFileFields";
-
-import { BasicInputDescription } from "./InputDescriptions";
-import type { BasicInputDescriptionProps } from "./InputDescriptions";
 
 export {
   AutoCompleteWithIcon,
@@ -39,6 +60,12 @@ export {
   TextAreaInputLabelProps,
   Logo,
   LogoProps,
+  WhiteBgSquareProgress,
+  WhiteBgSquareProgressProps,
+  DarkBgSquareProgress,
+  DarkBgSquareProgressProps,
+  NoBgSquareProgress,
+  NoBgSquareProgressProps,
   BasicTextArea,
   BasicTextAreaProps,
   BasicTextField,
