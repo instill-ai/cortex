d2c5331b0dfa5e3d280c8bca7789fc1136e38bae
pochun-chiu <pochun.chiu@instill.tech>
2022-04-21 14:21:43 +0800

    feat: add BasicInputDescription and its story

---
+++ src/components/InputDescriptions/BasicInputDescription/BasicInputDescription.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import InputDescriptionBase, {
+  InputDescriptionBaseProps,
+} from "../InputDescriptionBase";
+
+export type BasicInputDescriptionProps = Omit<
+  InputDescriptionBaseProps,
+  | "descriptionFontFamily"
+  | "descriptionFontSize"
+  | "descriptionLineHeight"
+  | "descriptionFontWeight"
+  | "descriptionTextColor"
+  | "marginTop"
+>;
+
+const BasicInputDescription: FC<BasicInputDescriptionProps> = (props) => {
+  return (
+    <InputDescriptionBase
+      description={props.description}
+      descriptionFontFamily="font-mono"
+      descriptionFontSize="text-sm"
+      descriptionLineHeight="leading-[15.6px]"
+      descriptionFontWeight="font-normal"
+      descriptionTextColor="text-instillGrey50"
+      marginTop="mt-2.5"
+    />
+  );
+};
+
+export default BasicInputDescription;
