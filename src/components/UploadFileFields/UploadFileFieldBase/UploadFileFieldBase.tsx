69b4d5219fd47df7a33b164be581a9df7336e63f
pochun-chiu <pochun.chiu@instill.tech>
2022-04-22 17:08:54 +0800

    feat: make UploadFileField container adapt the error message height

--- src/components/UploadFileFields/UploadFileFieldBase/UploadFileFieldBase.tsx
+++ src/components/UploadFileFields/UploadFileFieldBase/UploadFileFieldBase.tsx
@@ -4,24 +4,13 @@ import cn from "clsx";
 import { DocIcon } from "../../Icons";
 import InputLabelBase from "../../InputLabels/InputLabelBase";
 import InputDescriptionBase from "../../InputDescriptions/InputDescriptionBase";
+import { getElementPosition } from "../../../utils";
 
 export type UploadFileFieldBaseProps = Omit<
   BasicInputFieldAttributes,
   | "autoComplete"
-  | "disabledBgColor"
-  | "readOnlyBgColor"
   | "bgColor"
   | "inputBorderRadius"
-  | "disabledInputBgColor"
-  | "disabledInputBorderColor"
-  | "disabledInputBorderStyle"
-  | "disabledInputBorderWidth"
-  | "disabledInputTextColor"
-  | "readOnlyInputBgColor"
-  | "readOnlyInputBorderColor"
-  | "readOnlyInputBorderStyle"
-  | "readOnlyInputBorderWidth"
-  | "readOnlyInputTextColor"
   | "disabledCursor"
   | "readOnlyCursor"
   | "placeholderFontFamily"
@@ -103,18 +92,127 @@ const UploadFileFieldBase: React.FC<UploadFileFieldBaseProps> = ({
   descriptionFontWeight,
   descriptionLineHeight,
   descriptionTextColor,
+  errorInputBgColor,
+  errorInputBorderColor,
+  errorInputBorderStyle,
+  errorInputBorderWidth,
+  errorInputTextColor,
+  errorLabelFontFamily,
+  errorLabelFontSize,
+  errorLabelFontWeight,
+  errorLabelLineHeight,
+  errorLabelTextColor,
+  disabledInputBgColor,
+  disabledInputBorderColor,
+  disabledInputBorderStyle,
+  disabledInputBorderWidth,
+  disabledInputTextColor,
+  readOnlyInputBgColor,
+  readOnlyInputBorderColor,
+  readOnlyInputBorderStyle,
+  readOnlyInputBorderWidth,
+  readOnlyInputTextColor,
 }) => {
   const [answered, setAnswered] = React.useState(false);
   const [file, setFile] = React.useState<string>("");
+  const [inputLabelWidth, setInputLabelWidth] = React.useState<number>(null);
+  const [containerHeight, setContainerHeight] = React.useState<number>(null);
+
+  /**
+   * We use these ref to calculate the width and height of the container
+   * when there has very long error message which make label overflow.
+   * - When component is mount we calculate the label width
+   * - When error prop is changed we calculate the container height and compare it with original
+   *   container height, is former is greater, we adapt new container height
+   */
+
+  /**
+   * The wrapper of inputValueRef have a conditional rendering style, we use absolute bottom-5
+   * combine with inputLabel's top-5 to center the label and value, and we use mb-auto to shrink
+   * the wrapper itself
+   *
+   * ```js
+   *  inputLabelType === "inset"
+   *    ? cn("pt-8", containerHeight ? "absolute bottom-5" : "mb-auto")
+   *    : "my-auto"
+   * ```
+   *
+   */
+
+  const mainContainerRef = React.useRef<HTMLLabelElement>(null);
+  const uploadButtonRef = React.useRef<HTMLDivElement>(null);
+  const inputLabelRef = React.useRef<HTMLLabelElement>(null);
+  const inputValueRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    if (
+      !mainContainerRef.current ||
+      !uploadButtonRef.current ||
+      inputLabelType !== "inset"
+    ) {
+      return;
+    }
+
+    const mainContainerPosition = getElementPosition(mainContainerRef.current);
+    const uploadButtonPosition = getElementPosition(uploadButtonRef.current);
+
+    const inputLabelPaddingWidth = 20;
+
+    const inputLabelWidth =
+      mainContainerPosition.width -
+      uploadButtonPosition.width -
+      inputLabelPaddingWidth * 2;
+
+    console.log("re-calculate label width", inputLabelWidth);
+
+    setInputLabelWidth(inputLabelWidth);
+  }, [mainContainerRef, uploadButtonRef, inputLabelType]);
+
+  React.useEffect(() => {
+    if (
+      !error ||
+      !mainContainerRef ||
+      !inputLabelRef ||
+      !inputValueRef ||
+      !inputValueRef.current ||
+      inputLabelType !== "inset"
+    ) {
+      setContainerHeight(null);
+      return;
+    }
+
+    const inputLabelPosition = getElementPosition(inputLabelRef.current);
+    const mainContainerPosition = getElementPosition(mainContainerRef.current);
+    const inputValuePosition = getElementPosition(inputValueRef.current);
+
+    const inputLabelPaddingY = 20;
+    const gapBetweenLabelAndValue = 10;
+
+    const inputLabelHeight =
+      inputLabelPosition.height +
+      inputLabelPaddingY * 2 +
+      inputValuePosition.height +
+      gapBetweenLabelAndValue;
+
+    if (inputLabelHeight > mainContainerPosition.height) {
+      setContainerHeight(inputLabelHeight);
+    } else {
+      setContainerHeight(null);
+    }
+  }, [error, inputLabelRef, inputValueRef, inputLabelType]);
+
   return (
     <div className="flex flex-col">
       <div className="relative flex flex-col gap-y-2.5 group mb-2.5">
         <InputLabelBase
+          ref={inputLabelRef}
           answered={disabled ? true : readOnly ? true : answered}
           required={required}
           htmlFor={id}
           type={inputLabelType}
           label={label}
+          labelWidth={inputLabelWidth}
+          error={error}
           labelFontFamily={labelFontFamily}
           labelFontSize={labelFontSize}
           labelFontWeight={labelFontWeight}
@@ -122,20 +220,46 @@ const UploadFileFieldBase: React.FC<UploadFileFieldBaseProps> = ({
           labelTextColor={labelTextColor}
           labelActivateStyle={labelActivateStyle}
           labelDeActivateStyle={labelDeActivateStyle}
+          errorLabelFontFamily={errorLabelFontFamily}
+          errorLabelFontSize={errorLabelFontSize}
+          errorLabelFontWeight={errorLabelFontWeight}
+          errorLabelLineHeight={errorLabelLineHeight}
+          errorLabelTextColor={errorLabelTextColor}
         />
         <label
+          ref={mainContainerRef}
+          style={{ height: containerHeight ? `${containerHeight}px` : "" }}
           className={cn(
-            "flex flex-row border border-instillGrey15 p-0 cursor-pointer relative",
+            "flex flex-row p-0 cursor-pointer relative",
             inputWidth,
             inputHeight,
             inputBorderRadiusBottomLeft,
             inputBorderRadiusBottomRight,
             inputBorderRadiusTopLeft,
             inputBorderRadiusTopRight,
-            disabled
-              ? "instill-input-no-highlight border border-instillGrey20 border-dashed"
+            error
+              ? cn(
+                  errorInputBorderColor,
+                  errorInputBorderStyle,
+                  errorInputBorderWidth,
+                  errorInputBgColor
+                )
+              : disabled
+              ? cn(
+                  "instill-input-no-highlight",
+                  disabledInputBorderColor,
+                  disabledInputBorderStyle,
+                  disabledInputBorderWidth,
+                  disabledInputBgColor
+                )
               : readOnly
-              ? "instill-input-no-highlight border border-instillGrey20 border-solid"
+              ? cn(
+                  "instill-input-no-highlight",
+                  readOnlyInputBorderColor,
+                  readOnlyInputBorderStyle,
+                  readOnlyInputBorderWidth,
+                  readOnlyInputBgColor
+                )
               : focusHighlight
               ? cn(
                   inputBorderWidth,
@@ -154,40 +278,46 @@ const UploadFileFieldBase: React.FC<UploadFileFieldBaseProps> = ({
         >
           <div
             className={cn(
-              "flex flex-row mr-auto pl-5",
-              inputLabelType === "inset" ? "pt-6" : "my-auto"
+              "flex mr-auto pl-5",
+              inputLabelType === "inset"
+                ? cn("pt-8", containerHeight ? "absolute bottom-5" : "mb-auto")
+                : "my-auto"
             )}
           >
             {file ? (
-              <div className="flex gap-x-[5px]">
+              <div ref={inputValueRef} className="flex flex-row gap-x-2.5">
                 <DocIcon
                   width="w-5"
                   height="h-5"
                   position="my-auto"
                   color={
-                    disabled
-                      ? "text-instillGrey50"
+                    error
+                      ? errorInputTextColor
+                      : disabled
+                      ? disabledInputTextColor
                       : readOnly
-                      ? "text-instillGrey50"
+                      ? readOnlyInputTextColor
                       : inputTextColor
                   }
                 />
 
-                <p
+                <div
                   className={cn(
                     inputFontSize,
                     inputLineHeight,
                     inputFontWeight,
-                    disabled
-                      ? "text-instillGrey50"
+                    error
+                      ? errorInputTextColor
+                      : disabled
+                      ? disabledInputTextColor
                       : readOnly
-                      ? "text-instillGrey50"
+                      ? readOnlyInputTextColor
                       : inputTextColor,
-                    "my-auto"
+                    "flex"
                   )}
                 >
                   {file.split("\\").slice(-1)[0]}
-                </p>
+                </div>
               </div>
             ) : null}
           </div>
@@ -232,6 +362,7 @@ const UploadFileFieldBase: React.FC<UploadFileFieldBaseProps> = ({
             }}
           />
           <div
+            ref={uploadButtonRef}
             className={cn(
               "flex h-full ml-auto px-5",
               answered ? "absolute bottom-0 right-0 z-20" : "",
