4c19d8e432260f804a623e7973c689f7d096d47a
pochun-chiu <pochun.chiu@instill.tech>
2022-05-01 10:43:51 +0800

    feat: export SingleSelectOption

--- src/components/SingleSelects/index.ts
+++ src/components/SingleSelects/index.ts
@@ -1,4 +1,5 @@
 import BasicSingleSelect from "./BasicSingleSelect";
 import type { BasicSingleSelectProps } from "./BasicSingleSelect";
+import type { SingleSelectOption } from "./SingleSelectBase";
 
-export { BasicSingleSelect, BasicSingleSelectProps };
+export { BasicSingleSelect, BasicSingleSelectProps, SingleSelectOption };
