a001ff6ab99a40d5b153388cf7d4f506bc5d8b04
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-29 10:02:28 +0800

    feat: export react-select ActionMeta type

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ import { getTailwindClassNumber, getElementPosition } from "./utils";
 export { getTailwindClassNumber, getElementPosition };
 export * from "./ui";
 export * from "./generators";
+
+import { ActionMeta } from "react-select";
+export type { ActionMeta };
