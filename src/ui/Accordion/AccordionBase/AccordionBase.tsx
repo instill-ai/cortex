b100587111056846bbf1a2449bb208b24799af65
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-10-13 15:01:33 +0800

    refactor: refactor AccordionBase further to make it more readable

--- src/ui/Accordion/AccordionBase/AccordionBase.tsx
+++ src/ui/Accordion/AccordionBase/AccordionBase.tsx
@@ -2,60 +2,6 @@ import React from "react";
 import cn from "clsx";
 import { Nullable } from "../../../types/general";
 
-// In this file you may discover there have some duplications at the header
-// style of each accordion item. This is intended to make the code easier to
-// read and maintain. We had tried various ways including making the AccordionItem
-// component and extract its type. But it all ends up with lots of abstraction
-// using typescript Pick, Omit and keyof which are hard to read. We are open
-// to any kind of suggestion, but please don't change these codes using DRY
-// as its reasoning.
-
-// These duplications will be eliminated at the exported layer components.
-
-export type AccordionItemHeaderStyle = {
-  /** TailwindCSS format - The font style of the accordion item's header
-   * - e.g. font-sans
-   */
-  headerFont: string;
-
-  /** TailwindCSS format - The background color of the accordion item's header
-   * - e.g. bg-blue-500
-   */
-  headerBgColor: string;
-
-  /** TailwindCSS format - The text size of the accordion item's header
-   * - e.g. text-base
-   */
-  headerTextSize: string;
-
-  /** TailwindCSS format - The text color of the accordion item's header
-   * - e.g. text-blue-500
-   */
-  headerTextColor: string;
-
-  /** TailwindCSS format - The font weight of the accordion item's header
-   * - e.g. font-normal
-   */
-  headerFontWeight: string;
-
-  /** TailwindCSS format - The padding of the accordion item's header
-   * - e.g. p-5
-   */
-  headerPadding: Nullable<string>;
-
-  /**
-   * The header's icon when this accordion item is active. It will be put one
-   * the right side of the header regardless the icon's position
-   */
-  headerActiveIcon: React.ReactElement;
-
-  /**
-   * The header's icon when this accordion item is inactive. It will be put one
-   * the right side of the header regardless the icon's position
-   */
-  headerInActiveIcon: React.ReactElement;
-};
-
 export type AccordionItem = {
   /**
    * The background icon that will be displayed on the accordion item at a
@@ -65,37 +11,25 @@ export type AccordionItem = {
   bgIcon: Nullable<React.ReactElement>;
 
   /**
-   * The position of item icon.
-   */
-  bgIconPosition: Nullable<string>;
-
-  /**
    * The text of header.
    */
   header: string;
-
-  /**
-   * Whether enable header's icon or not.
-   */
-  enableHeaderIcon: boolean;
 
   /**
    * The content of the accordion item.
    */
   content: Nullable<React.ReactNode>;
 
-  /**
-   * The background color of the content.
+  /** TailwindCSS format - The text color of the accordion item's header
+   * - e.g. text-blue-500
    */
-  contentBgColor: string;
+  headerTextColor: string;
 
-  /**
-   * TailwindCSS format - The width of the content.
-   * - e.g. w-1/2
-   * - Please use this paired with `withIcon`
+  /** TailwindCSS format - The background color of the accordion item's header
+   * - e.g. bg-blue-500
    */
-  contentWidth: Nullable<string>;
-} & AccordionItemHeaderStyle;
+  headerBgColor: string;
+};
 
 export type AccordionBaseProps = {
   /**
@@ -119,10 +53,60 @@ export type AccordionBaseProps = {
    * The accordion items.
    */
   items: AccordionItem[];
+
+  /** TailwindCSS format - The font style of the accordion item's header
+   * - e.g. font-sans
+   */
+  headerFont: string;
+
+  /** TailwindCSS format - The text size of the accordion item's header
+   * - e.g. text-base
+   */
+  headerTextSize: string;
+
+  /** TailwindCSS format - The font weight of the accordion item's header
+   * - e.g. font-normal
+   */
+  headerFontWeight: string;
+
+  /** TailwindCSS format - The padding of the accordion item's header
+   * - e.g. p-5
+   */
+  headerPadding: Nullable<string>;
+
+  /**
+   * The header's icon when this accordion item is active. It will be put one
+   * the right side of the header regardless the icon's position
+   */
+  headerActiveIcon: React.ReactElement;
+
+  /**
+   * The header's icon when this accordion item is inactive. It will be put one
+   * the right side of the header regardless the icon's position
+   */
+  headerInActiveIcon: React.ReactElement;
+
+  /**
+   * The position of item icon.
+   */
+  bgIconPosition: Nullable<string>;
+
+  /**
+   * Whether enable header's icon or not.
+   */
+  enableHeaderIcon: boolean;
 };
 
 const AccordionBase = (props: AccordionBaseProps) => {
-  const { type, initialActiveIndex, items, itemGapY } = props;
+  const {
+    type,
+    initialActiveIndex,
+    items,
+    itemGapY,
+    bgIconPosition,
+    enableHeaderIcon,
+    ...headerStyle
+  } = props;
 
   const [activeIndex, setActiveIndex] = React.useState<number>(
     initialActiveIndex ? initialActiveIndex : 0
@@ -141,7 +125,7 @@ const AccordionBase = (props: AccordionBaseProps) => {
           {type === "withIcon" ? (
             <div
               onClick={() => setActiveIndex(i)}
-              className={cn("flex absolute cursor-pointer", e.bgIconPosition)}
+              className={cn("flex absolute cursor-pointer", bgIconPosition)}
             >
               {e.bgIcon}
             </div>
@@ -150,34 +134,31 @@ const AccordionBase = (props: AccordionBaseProps) => {
             onClick={() => setActiveIndex(i)}
             className={cn(
               "flex flex-row cursor-pointer",
-              e.headerPadding,
+              headerStyle.headerPadding,
               e.headerBgColor
             )}
           >
             <div
               className={cn(
                 "mr-auto",
-                e.headerFont,
-                e.headerTextSize,
                 e.headerTextColor,
-                e.headerFontWeight
+                headerStyle.headerFont,
+                headerStyle.headerTextSize,
+                headerStyle.headerFontWeight
               )}
             >
               {e.header}
             </div>
-            {e.enableHeaderIcon ? (
+            {enableHeaderIcon ? (
               <>
-                {i === activeIndex ? e.headerActiveIcon : e.headerInActiveIcon}
+                {i === activeIndex
+                  ? headerStyle.headerActiveIcon
+                  : headerStyle.headerInActiveIcon}
               </>
             ) : null}
           </div>
-          <div className={cn("w-full", e.contentBgColor)}>
-            <div
-              className={cn(
-                e.contentWidth,
-                i === activeIndex ? "flex" : "hidden"
-              )}
-            >
+          <div className="w-full">
+            <div className={i === activeIndex ? "flex" : "hidden"}>
               {e.content}
             </div>
           </div>
