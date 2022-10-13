6a0089abc27b7bc11a717c8bbf2ff6e0bde232fd
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-10-13 13:19:58 +0800

    feat: add comment of accordion item

--- src/ui/Accordion/AccordionBase/AccordionItem/AccordionItem.tsx
+++ src/ui/Accordion/AccordionBase/AccordionItem/AccordionItem.tsx
@@ -4,30 +4,109 @@ import { Nullable } from "../../../../types/general";
 
 export type AccordionItemHeaderStyleProps = {
   /** TailwindCSS format - The font style of the accordion item's header
-   * - e.g. bg-blue-100
+   * - e.g. font-sans
    */
   headerFont: string;
+
+  /** TailwindCSS format - The background color of the accordion item's header
+   * - e.g. bg-blue-500
+   */
   headerBgColor: string;
+
+  /** TailwindCSS format - The text size of the accordion item's header
+   * - e.g. text-base
+   */
   headerTextSize: string;
+
+  /** TailwindCSS format - The text color of the accordion item's header
+   * - e.g. text-blue-500
+   */
   headerTextColor: string;
+
+  /** TailwindCSS format - The font weight of the accordion item's header
+   * - e.g. font-normal
+   */
   headerFontWeight: string;
+
+  /** TailwindCSS format - The padding of the accordion item's header
+   * - e.g. p-5
+   */
   headerPadding: Nullable<string>;
+
+  /**
+   * The header's icon when this accordion item is active. It will be put one
+   * the right side of the header regardless the icon's position
+   */
   headerActiveIcon: React.ReactElement;
+
+  /**
+   * The header's icon when this accordion item is inactive. It will be put one
+   * the right side of the header regardless the icon's position
+   */
   headerInActiveIcon: React.ReactElement;
 };
 
 export type AccordionItemProps = {
+  /**
+   * We have two type of accordion item right now. `withIcon` will display the
+   * specific item icon consumer indicated at a absolute position. `basic` will
+   * not display this item icon.
+   */
   type: "withIcon" | "basic";
-  header: string;
-  enableHeaderIcon: boolean;
-  contentBgColor: string;
-  selfIndex: number;
-  activeIndex: number;
-  setActiveIndex: React.Dispatch<React.SetStateAction<number>>;
-  contentWidth: Nullable<string>;
-  content: Nullable<React.ReactNode>;
+
+  /**
+   * The icon that will be displayed on the accordion item at a absolute position.
+   * Please use itemIconContainerPosition to indicate the position.
+   */
   itemIcon: Nullable<React.ReactElement>;
+
+  /**
+   * The position of item icon.
+   */
   itemIconContainerPosition: Nullable<string>;
+
+  /**
+   * The text of header.
+   */
+  header: string;
+
+  /**
+   * Whether enable header's icon or not.
+   */
+  enableHeaderIcon: boolean;
+
+  /**
+   * The content of the accordion item.
+   */
+  content: Nullable<React.ReactNode>;
+
+  /**
+   * The background color of the content.
+   */
+  contentBgColor: string;
+
+  /**
+   * TailwindCSS format - The width of the content.
+   * - e.g. w-1/2
+   * - Please use this paired with `withIcon`
+   */
+  contentWidth: Nullable<string>;
+
+  /**
+   * The index of the accordion item
+   */
+  selfIndex: number;
+
+  /**
+   * Current active index of the accordion
+   */
+  activeIndex: number;
+
+  /**
+   * React dispatch function that change the current active index of the
+   * accordion
+   */
+  setActiveIndex: React.Dispatch<React.SetStateAction<number>>;
 } & AccordionItemHeaderStyleProps;
 
 const AccordionItem = (props: AccordionItemProps) => {
