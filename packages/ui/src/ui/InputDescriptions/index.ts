54ff80a72041ec60ce1623a6a1618d1083e7ffea
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-02-24 15:33:03 +0800

    fix: fix lint duplicated import and non-interactive element issue

--- packages/ui/src/ui/InputDescriptions/index.ts
+++ packages/ui/src/ui/InputDescriptions/index.ts
@@ -1,14 +1,1 @@
-import BasicInputDescription from "./BasicInputDescription";
-import { basicInputDescriptionConfig } from "./BasicInputDescription";
-import type {
-  BasicInputDescriptionProps,
-  BasicInputDescriptionOmitKeys,
-  BasicInputDescriptionConfig,
-} from "./BasicInputDescription";
-
-export { BasicInputDescription, basicInputDescriptionConfig };
-export type {
-  BasicInputDescriptionProps,
-  BasicInputDescriptionOmitKeys,
-  BasicInputDescriptionConfig,
-};
+export * from "./BasicInputDescription";
