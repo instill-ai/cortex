c67ee44f60b73fc10d70772314ebb18a681d34d8
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-06 20:52:36 +0800

    refactor: rename sdk to vdp-sdk

--- packages/toolkit/src/type/general.ts
+++ packages/toolkit/src/type/general.ts
@@ -1,3 +1,7 @@
+import { Dispatch, SetStateAction } from "react";
+
 export type Nullable<T> = T | null;
 
 export type AtLeast<T, K extends keyof T> = Partial<T> & Pick<T, K>;
+
+export type UseCustomHookResult<T> = [T, Dispatch<SetStateAction<T>>];
