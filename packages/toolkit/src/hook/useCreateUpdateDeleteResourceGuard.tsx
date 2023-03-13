9441065319a6eb05abe439eae9279b800ed0a561
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-13 15:05:17 +0800

    feat: add hooks

---
+++ packages/toolkit/src/hook/useCreateUpdateDeleteResourceGuard.tsx
@@ -0,0 +1,16 @@
+import { env } from "../utility";
+import { useEffect, useState } from "react";
+
+export function useCreateUpdateDeleteResourceGuard() {
+  const [enable, setEnable] = useState(false);
+
+  useEffect(() => {
+    setEnable(
+      env("NEXT_PUBLIC_DISABLE_CREATE_UPDATE_DELETE_RESOURCE") === "true"
+        ? true
+        : false
+    );
+  }, []);
+
+  return enable;
+}
---
+++ packages/toolkit/src/hook/useWindowSize.ts
@@ -0,0 +1,44 @@
+import { Nullable } from "../type";
+import { useState, useEffect } from "react";
+
+export type WindowSize = {
+  width: number;
+  height: number;
+};
+
+export function useWindowSize(): Nullable<WindowSize> {
+  const [windowSize, setWindowSize] = useState<Nullable<WindowSize>>(null);
+
+  const getWindowSize = (): { width: number; height: number } => {
+    const width =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
+
+    const height =
+      window.innerHeight ||
+      document.documentElement.clientHeight ||
+      document.body.clientHeight;
+
+    return {
+      width,
+      height,
+    };
+  };
+
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    setWindowSize(getWindowSize());
+    const handleResize = () => {
+      setWindowSize(getWindowSize());
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+}
