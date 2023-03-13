67b1730b1e1c5f19ec35832ec2672ec3ec71e86d
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-13 23:04:11 +0800

    feat: add configure pipeline form (#169)
    Because
    
    - centralize components
    
    This commit
    
    - add configure pipeline form

--- packages/view/src/mgmt/ConfigureProfileForm/ConfigureProfileForm.tsx
+++ packages/view/src/mgmt/ConfigureProfileForm/ConfigureProfileForm.tsx
@@ -1,11 +1,10 @@
 import { Nullable } from "@instill-ai/toolkit";
-import { FormRoot } from "@instill-ai/design-system";
 import { FirstNameField } from "./FirstNameField";
 import { LastNameField } from "./LastNameField";
 import { OrgNameField } from "./OrgNameField";
 import { RoleField } from "./RoleField";
 import { UserNameField } from "./UserNameField";
-import { SingleSelectOption } from "@instill-ai/design-system";
+import { SingleSelectOption, FormRoot } from "@instill-ai/design-system";
 import { NewsletterSubscriptionField } from "./NewsletterSubscriptionField";
 import { ConfigureProfileControl } from "./ConfigureProfileControl";
 
@@ -22,7 +21,7 @@ export const ConfigureProfileForm = ({
 }: ConfigureProfileFormProps) => {
   return (
     <FormRoot marginBottom={marginBottom} formLess={false} width={width}>
-      <div className="mb-6 flex flex-row p-10">
+      <div className="mb-6 flex flex-row py-10">
         <div className="h-16 w-16 rounded-full bg-instillGrey20">{}</div>
       </div>
       <div className="mb-8 flex flex-col gap-y-6">
