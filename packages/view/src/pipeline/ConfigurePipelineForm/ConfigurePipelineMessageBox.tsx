490da781e81e94363fd76eaa9f307f02aa985bc4
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-13 20:34:41 +0800

    feat: add messagebox

---
+++ packages/view/src/pipeline/ConfigurePipelineForm/ConfigurePipelineMessageBox.tsx
@@ -0,0 +1,27 @@
+import { BasicProgressMessageBox } from "@instill-ai/design-system";
+import { MessageBoxStore, useMessageBoxStore } from "@instill-ai/toolkit";
+import { shallow } from "zustand/shallow";
+
+const selector = (state: MessageBoxStore) => ({
+  status: state.status,
+  message: state.message,
+  description: state.description,
+  activate: state.activate,
+  setStateValue: state.setStateValue,
+});
+
+export const ConfigurePipelineMessageBox = () => {
+  const { status, message, description, activate, setStateValue } =
+    useMessageBoxStore(selector, shallow);
+
+  return (
+    <div className="flex">
+      <BasicProgressMessageBox
+        state={{ status, message, description, activate }}
+        setState={setMessageBoxState}
+        width="w-[25vw]"
+        closable={true}
+      />
+    </div>
+  );
+};
