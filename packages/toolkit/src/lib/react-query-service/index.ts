bb6b274e9d2f533381e047706fd29b6657598dcd
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-20 20:18:34 +0800

    fix: fix authorization header issue (#229)
    Because
    
    - The way we set the authorization header is in-correct
    
    This commit
    
    - fix authorization header issue

--- packages/toolkit/src/lib/react-query-service/index.ts
+++ packages/toolkit/src/lib/react-query-service/index.ts
@@ -3,3 +3,10 @@ export * from "./helper";
 export * from "./mgmt";
 export * from "./model";
 export * from "./pipeline";
+
+/** Re export react-query to solve ESM, CJS confict issue */
+export {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
