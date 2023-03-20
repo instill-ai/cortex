ed12589f880a7c6b9207baaa6702640fa2d10f05
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-20 22:32:00 +0800

    feat: add query provide into cortex-playground

--- apps/cortex-playground/src/pages/_app.tsx
+++ apps/cortex-playground/src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@instill-ai/toolkit";
+import type { AppProps } from "next/app";
+
+export const queryCache = new QueryCache();
+export const queryClient = new QueryClient({ queryCache });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />;
+    </QueryClientProvider>
+  );
 }
