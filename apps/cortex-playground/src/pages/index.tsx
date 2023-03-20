3e8d928942ce4b12bd12da858a1d89bf888a34fc
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-20 14:51:15 +0800

    feat: add cortex-playground (#218)
    Because
    
    - We need a place to test our component manually
    
    This commit
    
    - add cortex-playground

---
+++ apps/cortex-playground/src/pages/index.tsx
@@ -0,0 +1,50 @@
+import { Root } from "@/components/Root";
+import { ConfigureProfileForm } from "@instill-ai/toolkit";
+
+const IndexPage = () => {
+  return (
+    <Root>
+      <ConfigureProfileForm
+        marginBottom={null}
+        roles={[
+          {
+            label: "Manager (who makes decisions)",
+            value: "manager",
+          },
+          {
+            label:
+              "AI Researcher (who devises ML algorithms, trains and evaluates models)",
+            value: "ai-researcher",
+          },
+          {
+            label:
+              "AI Engineer (who prepares dataset and makes models delivered by AI Researchers production-ready)",
+            value: "ai-engineer",
+          },
+          {
+            label:
+              "Data Engineer (who builds data pipeline for data analytics or applications)",
+            value: "data-engineer",
+          },
+          {
+            label:
+              "Data Scientist (who analyses data for distilling business value)",
+            value: "data-scientist",
+          },
+          {
+            label:
+              "Analytics Engineer (who possesses skills of both Data Scientist and Data Engineer)",
+            value: "analytics-engineer",
+          },
+          {
+            label: "Hobbyist (I love AI!)",
+            value: "hobbyist",
+          },
+        ]}
+        width="w-[600px]"
+      />
+    </Root>
+  );
+};
+
+export default IndexPage;
