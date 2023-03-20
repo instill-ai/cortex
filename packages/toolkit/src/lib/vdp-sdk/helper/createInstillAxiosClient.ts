3e265de246db5080b9a1d94518aa89ceb0b101fd
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-20 20:12:08 +0800

    fix: fix authorization header issue

--- packages/toolkit/src/lib/vdp-sdk/helper/createInstillAxiosClient.ts
+++ packages/toolkit/src/lib/vdp-sdk/helper/createInstillAxiosClient.ts
@@ -3,12 +3,16 @@ import axios from "axios";
 import { Nullable } from "../../type";
 
 export const createInstillAxiosClient = (accessToken: Nullable<string>) => {
+  const headers = accessToken
+    ? {
+        Authorization: `Bearer ${accessToken}`,
+      }
+    : {};
+
   return axios.create({
     baseURL: `${env("NEXT_PUBLIC_API_GATEWAY_BASE_URL")}/${env(
       "NEXT_PUBLIC_API_VERSION"
     )}`,
-    headers: {
-      Authorization: accessToken ? `Bearer ${accessToken}` : undefined,
-    },
+    headers,
   });
 };
