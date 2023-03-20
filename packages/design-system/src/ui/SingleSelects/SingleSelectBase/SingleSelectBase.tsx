5ec3865043e4a67ff5ac5b26624a93327a6b08b3
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-20 15:29:55 +0800

    fix: fix SingleSelectBase wrong prop issue

--- packages/design-system/src/ui/SingleSelects/SingleSelectBase/SingleSelectBase.tsx
+++ packages/design-system/src/ui/SingleSelects/SingleSelectBase/SingleSelectBase.tsx
@@ -227,9 +227,9 @@ export const SingleSelectBase: React.FC<SingleSelectBaseProps> = (props) => {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <polyline points="6 9 12 15 18 9"></polyline>
                 </svg>
