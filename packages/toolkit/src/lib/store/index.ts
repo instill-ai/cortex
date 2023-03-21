753a9e8181bba036d11c88ece59f9ef3c07476fe
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-21 16:46:04 +0800

    chore: remove useCreateSourceFormStore

--- packages/toolkit/src/lib/store/index.ts
+++ packages/toolkit/src/lib/store/index.ts
@@ -5,4 +5,3 @@ export * from "./useConfigureSourceFormStore";
 export * from "./useCreateResourceFormStore";
 export * from "./useMessageBoxStore";
 export * from "./useModalStore";
-export * from "./useCreateSourceFormStore";
