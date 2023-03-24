7adc2e1ae932c9df75b6b7a3828e041994d70786
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-24 16:34:16 +0800

    feat: [INS-315] add sources table into toolkit (#257)
    Because
    
    - centralize components
    
    This commit
    
    - add sources table into toolkit

---
+++ packages/toolkit/src/view/source/SourcesTable.tsx
@@ -0,0 +1,127 @@
+import { useMemo, useState } from "react";
+
+import {
+  ConnectionTypeCell,
+  PipelinesCell,
+  NameCell,
+  TableHeadItem,
+  TableHead,
+  TableLoadingProgress,
+  StateOverview,
+  PaginationListContainer,
+} from "../../components";
+import {
+  useSearchedResources,
+  useStateOverviewCounts,
+  chunk,
+  env,
+  type SourceWithPipelines,
+  type Nullable,
+} from "../../lib";
+import { SourceTablePlaceholder } from "./SourceTablePlaceholder";
+
+export type SourcesTableProps = {
+  sources: Nullable<SourceWithPipelines[]>;
+  marginBottom: Nullable<string>;
+};
+
+export const SourcesTable = ({ sources, marginBottom }: SourcesTableProps) => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [searchTerm, setSearchTerm] = useState<Nullable<string>>(null);
+
+  const searchedSources = useSearchedResources({
+    resources: sources || null,
+    searchTerm,
+  });
+
+  const searchedPipelinePages = useMemo(() => {
+    return chunk(searchedSources, env("NEXT_PUBLIC_LIST_PAGE_SIZE"));
+  }, [searchedSources]);
+
+  const stateOverviewCounts = useStateOverviewCounts(searchedSources);
+
+  const tableHeadItems = useMemo<TableHeadItem[]>(() => {
+    return [
+      {
+        key: "connector-state-overview-head",
+        item: (
+          <StateOverview
+            errorCounts={stateOverviewCounts?.error || 0}
+            offlineCounts={stateOverviewCounts?.offline || 0}
+            onlineCounts={stateOverviewCounts?.online || 0}
+          />
+        ),
+      },
+      {
+        key: "connector-type-head",
+        item: "Source",
+      },
+      {
+        key: "connector-pipelines-head",
+        item: "Pipelines",
+      },
+    ];
+  }, [stateOverviewCounts]);
+
+  return (
+    <PaginationListContainer
+      title="Source"
+      description="These are the sources you can select"
+      currentPage={currentPage}
+      setCurrentPage={setCurrentPage}
+      searchTerm={searchTerm}
+      setSearchTerm={setSearchTerm}
+      totalPage={searchedPipelinePages.length}
+      displaySearchField={sources?.length !== 0 ? true : false}
+      marginBottom={marginBottom}
+    >
+      {sources ? (
+        sources.length === 0 ? (
+          <SourceTablePlaceholder
+            enablePlaceholderCreateButton={false}
+            marginBottom={null}
+          />
+        ) : (
+          <table className="table-auto border-collapse">
+            <TableHead
+              borderColor="border-instillGrey20"
+              bgColor="bg-instillGrey05"
+              items={tableHeadItems}
+            />
+            <tbody>
+              {searchedPipelinePages[currentPage]
+                ? searchedPipelinePages[currentPage].map((source) => (
+                    <tr
+                      key={source.name}
+                      className="bg-white border border-instillGrey20"
+                    >
+                      <NameCell
+                        name={source.id}
+                        width=""
+                        state="STATE_ONLINE"
+                        padding="py-5 pl-6"
+                        link={`/sources/${source.id}`}
+                      />
+                      <ConnectionTypeCell
+                        connectorDefinition={source.source_connector_definition}
+                        connectorName={source.id}
+                        width=""
+                        padding="py-5"
+                      />
+                      <PipelinesCell
+                        width=""
+                        padding="py-5 pr-6"
+                        pipelineCount={source.pipelines.length}
+                      />
+                    </tr>
+                  ))
+                : null}
+            </tbody>
+          </table>
+        )
+      ) : (
+        <TableLoadingProgress marginBottom={null} />
+      )}
+    </PaginationListContainer>
+  );
+};
--- packages/toolkit/src/view/source/index.ts
+++ packages/toolkit/src/view/source/index.ts
@@ -1,3 +1,4 @@
 export * from "./CreateSourceForm";
 export * from "./ConfigureSourceForm";
 export * from "./SourceTablePlaceholder";
+export * from "./SourcesTable";
