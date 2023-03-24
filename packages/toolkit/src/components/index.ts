c864566fe2ab3d9070344055c4435b038585cae4
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-24 13:58:15 +0800

    feat: add NameCell, StateIcon and StateLabel

--- packages/toolkit/src/components/index.ts
+++ packages/toolkit/src/components/index.ts
@@ -2,3 +2,6 @@ export * from "./TablePlaceholderBase";
 export * from "./ImageWithFallback";
 export * from "./DeleteResourceModal";
 export * from "./PaginationListContainer";
+export * from "./StateIcon";
+export * from "./StateLabel";
+export * from "./NameCell";
