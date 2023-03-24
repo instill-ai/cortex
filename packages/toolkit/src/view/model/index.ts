2ee3f3068aabd182a3c162a599e080fda9ff48c7
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-03-24 13:12:25 +0800

    feat: add ModelTablePlaceholder

--- packages/toolkit/src/view/model/index.ts
+++ packages/toolkit/src/view/model/index.ts
@@ -2,3 +2,4 @@ export * from "./ChangeModelInstanceStateToggle";
 export * from "./CreateModelWithPresetForm";
 export * from "./ConfigureModelForm";
 export * from "./ConfigureModelInstanceForm";
+export * from "./ModelTablePlaceholder";
