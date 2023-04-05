c32c5356b87199ef0cd286ec90618b7fa7b2d8cb
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-04-05 16:32:41 +0800

    fix: fix select overflow issue

--- packages/design-system/src/ui/SingleSelects/SingleSelectBase/SingleSelectBase.stories.tsx
+++ packages/design-system/src/ui/SingleSelects/SingleSelectBase/SingleSelectBase.stories.tsx
@@ -15,22 +15,26 @@ export default meta;
 
 const Template: StoryFn<typeof SingleSelectBase> = (args) => {
   const optionsWithoutIcon: SingleSelectOption[] = [
-    {
-      value: "grpc",
-      label: "gRPC-gRPC-gRPC-gRPC-gRPC-gRPC-gRPC-gRPC-gRPC-",
-    },
-    {
-      value: "http",
-      label: "HTTP",
-    },
-    {
-      value: "snowflake",
-      label: "Snowflake",
-    },
-    {
-      value: "mongodb",
-      label: "MongoDB",
-    },
+    { label: "Option 1", value: "option-1" },
+    { label: "Option 2", value: "option-2" },
+    { label: "Option 3", value: "option-3" },
+    { label: "Option 4", value: "option-4" },
+    { label: "Option 5", value: "option-5" },
+    { label: "Option 6", value: "option-6" },
+    { label: "Option 7", value: "option-7" },
+    { label: "Option 8", value: "option-8" },
+    { label: "Option 9", value: "option-9" },
+    { label: "Option 10", value: "option-10" },
+    { label: "Option 11", value: "option-11" },
+    { label: "Option 12", value: "option-12" },
+    { label: "Option 13", value: "option-13" },
+    { label: "Option 14", value: "option-14" },
+    { label: "Option 15", value: "option-15" },
+    { label: "Option 16", value: "option-16" },
+    { label: "Option 17", value: "option-17" },
+    { label: "Option 18", value: "option-18" },
+    { label: "Option 19", value: "option-19" },
+    { label: "Option 20", value: "option-20" },
   ];
 
   const onChange = (option: Nullable<SingleSelectOption>) => {
