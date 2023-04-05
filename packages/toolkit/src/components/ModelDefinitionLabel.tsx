7a02a219a903f0d2439c797221cdf038db7ce8dd
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-04-05 21:53:53 +0800

    feat: add ModelDefinitionLabel

--- packages/toolkit/src/components/ModelDefinitionLabel.tsx
+++ packages/toolkit/src/components/ModelDefinitionLabel.tsx
@@ -1,18 +1,18 @@
 import cn from "clsx";
-import { getModelInstanceTaskToolkit } from "@instill-ai/design-system";
+import { getModelDefinitionToolkit } from "@instill-ai/design-system";
 import { Nullable } from "../lib";
 
-export type ModelTaskLabelProps = {
-  task: Nullable<string>;
+export type ModelDefinitionLabelProps = {
+  modelDefinition: Nullable<string>;
   marginBottom: Nullable<string>;
   position: Nullable<string>;
 };
 
-export const ModelTaskLabel = ({
-  task,
+export const ModelDefinitionLabel = ({
+  modelDefinition,
   marginBottom,
   position,
-}: ModelTaskLabelProps) => {
+}: ModelDefinitionLabelProps) => {
   const iconStyle = {
     width: "w-[18px]",
     height: "h-[18px]",
@@ -20,29 +20,29 @@ export const ModelTaskLabel = ({
     color: "fill-instillGrey95",
   };
 
-  if (!task) {
+  if (!modelDefinition) {
     return (
       <div
         className={cn("flex gap-x-2 bg-white p-2", marginBottom, position)}
-        data-testid="model-task-label"
+        data-testid="model-definition-label"
       >
         <div className={cn(iconStyle.width, iconStyle.height)} />
-        <p className="my-auto flex capitalize text-instillGrey90 text-instill-small">
+        <p className="my-auto flex text-instillGrey90 text-instill-small">
           Unspecified
         </p>
       </div>
     );
   }
 
-  const toolkit = getModelInstanceTaskToolkit(task);
+  const toolkit = getModelDefinitionToolkit(modelDefinition);
 
   return (
     <div
       className={cn("flex gap-x-2 bg-white p-2", marginBottom, position)}
-      data-testid="model-task-label"
+      data-testid="model-definition-label"
     >
       {toolkit.getIcon(iconStyle)}
-      <p className="my-auto flex capitalize text-instillGrey90 text-instill-small">
+      <p className="my-auto flex text-instillGrey90 text-instill-small">
         {toolkit.label}
       </p>
     </div>
