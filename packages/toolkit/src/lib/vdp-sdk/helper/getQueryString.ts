016d3ab288355f41c4f5896a37539d8707ce2ff0
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-04-28 13:03:42 +0800

    feat: update getQueryString to cover more scenario and add tests

--- packages/toolkit/src/lib/vdp-sdk/helper/getQueryString.ts
+++ packages/toolkit/src/lib/vdp-sdk/helper/getQueryString.ts
@@ -5,15 +5,29 @@ export const getQueryString = (
   pageSize: Nullable<number>,
   nextPageToken: Nullable<string>
 ) => {
-  let queryString = baseUrl;
+  let url = baseUrl;
+
+  if (pageSize || nextPageToken) {
+    // Check if the baseUrl already has a query string
+    if (baseUrl.includes("?")) {
+      url += "&";
+    } else {
+      url += "?";
+    }
+  }
 
   if (pageSize) {
-    queryString += `&page_size=${pageSize}`;
+    url += `pageSize=${pageSize}&`;
   }
 
   if (nextPageToken) {
-    queryString += `&page_token=${nextPageToken}`;
+    url += `pageToken=${nextPageToken}&`;
   }
 
-  return queryString;
+  // Remove the trailing '&' if there are any query parameters
+  if (url.endsWith("&")) {
+    url = url.slice(0, -1);
+  }
+
+  return url;
 };
