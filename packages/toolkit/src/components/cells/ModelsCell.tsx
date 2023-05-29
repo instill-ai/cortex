9278693b3a9657897ed482db04a030593c93cdaa
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-05-29 15:36:01 +0800

    fix: fix modelsCell taks wrong field as modelName

--- packages/toolkit/src/components/cells/ModelsCell.tsx
+++ packages/toolkit/src/components/cells/ModelsCell.tsx
@@ -12,17 +12,28 @@ export const ModelsCell = ({ width, models, padding }: ModelsCellProps) => {
   return (
     <td>
       <div className={cn("flex flex-col gap-y-4", width, padding)}>
-        {models.map((model) => (
-          <div key={model.id} className="flex flex-row gap-x-1">
-            <ModelInstanceIcon
-              width="w-[30px]"
-              height="h-[30px]"
-              color="fill-black"
-              position="my-auto"
-            />
-            <p className="my-auto text-black text-instill-small">{model.id}</p>
-          </div>
-        ))}
+        {models.map((model) => {
+          let modelName: Nullable<string> = null;
+          if ("resource_name" in model) {
+            modelName = model.resource_name;
+          } else {
+            modelName = model.name;
+          }
+
+          return (
+            <div key={model.id} className="flex flex-row gap-x-1">
+              <ModelInstanceIcon
+                width="w-[30px]"
+                height="h-[30px]"
+                color="fill-black"
+                position="my-auto"
+              />
+              <p className="my-auto text-black text-instill-small">
+                {modelName}
+              </p>
+            </div>
+          );
+        })}
       </div>
     </td>
   );
