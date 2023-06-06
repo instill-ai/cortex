4b1edeffa3b9d032404cb8e23ef29590108f14fe
Po-Chun Chiu <57251712+EiffelFly@users.noreply.github.com>
2023-06-06 19:50:30 +0800

    feat(design-system): add multiple icons into design-system (#461)
    Because
    
    - We have new icons
    
    This commit
    
    - Add BarChartSquare02, Box,  ChevronDown, ChevronUp, HelpCircle

--- packages/design-system/src/new-ui/Icons/index.ts
+++ packages/design-system/src/new-ui/Icons/index.ts
@@ -1,9 +1,19 @@
-import { Gear01 } from "./Gear01";
+import { BarChartSquare02 } from "./BarChartSquare02";
+import { Box } from "./Box";
+import { ChevronDown } from "./ChevronDown";
+import { ChevronUp } from "./ChevronUp";
 import { Database01 } from "./Database01";
+import { Gear01 } from "./Gear01";
+import { HelpCircle } from "./HelpCircle";
 import { Logout01 } from "./Logout01";
 
 export const Icons = {
-  Gear01,
+  BarChartSquare02,
+  Box,
+  ChevronDown,
+  ChevronUp,
   Database01,
+  Gear01,
+  HelpCircle,
   Logout01,
 };
