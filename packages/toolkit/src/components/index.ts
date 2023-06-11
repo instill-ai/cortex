a2278c6c4d332b197fea837b1b968f28ce3b5e73
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-06-11 22:30:32 +0800

    chore: remove PageBase from toolkit

--- packages/toolkit/src/components/index.ts
+++ packages/toolkit/src/components/index.ts
@@ -8,7 +8,6 @@ export * from "./TableLoadingProgress";
 export * from "./StateOverview";
 export * from "./TableHead";
 export * from "./ModelDefinitionLabel";
-export * from "./PageBase";
 export * from "./ModelTaskLabel";
 export * from "./cells";
 export * from "./TableError";
