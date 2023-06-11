f1b853968f523f3f365fbf7a67e874d7dd9985e2
Po-Chun Chiu <57251712+EiffelFly@users.noreply.github.com>
2023-06-11 21:55:42 +0800

    feat: add PageBase component into toolkit (#481)
    Because
    
    - Centralize components
    
    This commit
    
    - add PageBase component into toolkit

--- packages/toolkit/src/components/index.ts
+++ packages/toolkit/src/components/index.ts
@@ -8,6 +8,7 @@ export * from "./TableLoadingProgress";
 export * from "./StateOverview";
 export * from "./TableHead";
 export * from "./ModelDefinitionLabel";
+export * from "./PageBase";
 export * from "./ModelTaskLabel";
 export * from "./cells";
 export * from "./TableError";
--- packages/toolkit/tailwind.config.js
+++ packages/toolkit/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require("@instill-ai/design-tokens/dist/tailwind.config.cjs")],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
