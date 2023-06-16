515685e6cd34a7823812c4ee652952a564fb7fb4
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-06-16 21:47:42 +0800

    chore: test no workaround

--- packages/design-system/.storybook/preview.ts
+++ packages/design-system/.storybook/preview.ts
@@ -4,7 +4,7 @@ import "tailwindcss/tailwind.css";
 import "../src/styles/global.css";
 import root from "@instill-ai/design-tokens/dist/theme/root.css";
 import light from "@instill-ai/design-tokens/dist/theme/light.css";
-import dark from "@instill-ai/design-tokens/dist/theme/dark.css";
+import "@instill-ai/design-tokens/dist/theme/dark.css";
 
 const preview: Preview = {
   parameters: {
