b909490e923ba570dbeae5fecab266df46f31b8a
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-06-16 21:49:46 +0800

    chore: uncomment the tailwind configuration preset

--- packages/design-system/.storybook/preview.ts
+++ packages/design-system/.storybook/preview.ts
@@ -2,9 +2,13 @@ import { Preview } from "@storybook/react";
 
 import "tailwindcss/tailwind.css";
 import "../src/styles/global.css";
+
+// This is a hack to get the design tokens to work in Storybook.
+// Storybook use Webpack under the hood, but it doesn't respect
+// the sideEffects configuration in the design-tokens
 import root from "@instill-ai/design-tokens/dist/theme/root.css";
 import light from "@instill-ai/design-tokens/dist/theme/light.css";
-import "@instill-ai/design-tokens/dist/theme/dark.css";
+import dark from "@instill-ai/design-tokens/dist/theme/dark.css";
 
 const preview: Preview = {
   parameters: {
