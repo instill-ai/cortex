81d3a22eb684363d255995ea40f9aae5b1c638eb
Po-Chun Chiu <57251712+EiffelFly@users.noreply.github.com>
2023-06-16 14:19:09 +0800

    feat(design-system): add new Textarea component (#490)
    Because
    
    - we are re-designing the design-system
    
    This commit
    
    - add new Textarea component

---
+++ packages/design-system/src/new-ui/Textarea/Textarea.tsx
@@ -0,0 +1,23 @@
+import cn from "clsx";
+import * as React from "react";
+
+export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          "flex min-h-[80px] product-body-text-2-regular w-full rounded-sm border bg-transparent p-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-semantic-accent-default focus-visible:border-semantic-accent-default disabled:cursor-not-allowed disabled:opacity-80",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Textarea.displayName = "Textarea";
+
+export { Textarea };
