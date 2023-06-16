8e891f1ed23ad76d88b6f7ea8bbea342c1669d1c
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-06-16 16:18:18 +0800

    feat: update dependency

--- packages/design-system/.storybook/preview.ts
+++ packages/design-system/.storybook/preview.ts
@@ -2,6 +2,9 @@ import { Preview } from "@storybook/react";
 
 import "tailwindcss/tailwind.css";
 import "../src/styles/global.css";
+import "@instill-ai/design-tokens/dist/theme/root.css";
+import "@instill-ai/design-tokens/dist/theme/light.css";
+import "@instill-ai/design-tokens/dist/theme/dark.css";
 
 const preview: Preview = {
   parameters: {
