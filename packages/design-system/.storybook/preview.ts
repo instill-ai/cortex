b5f406dd8841db30571650d00e3c7d34502f0605
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-06-16 15:52:19 +0800

    fix: experiment preview.ts

---
+++ packages/design-system/.storybook/preview.ts
@@ -0,0 +1,21 @@
+import { Preview } from "@storybook/react";
+
+import "tailwindcss/tailwind.css";
+import "../src/styles/global.css";
+import "@instill-ai/design-tokens/dist/theme/root.css";
+import "@instill-ai/design-tokens/dist/theme/light.css";
+import "@instill-ai/design-tokens/dist/theme/dark.css";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
