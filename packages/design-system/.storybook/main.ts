fcbf5e9fdcc64ed6ea63f6359c8938ff716084a8
Po-Chun Chiu <57251712+EiffelFly@users.noreply.github.com>
2023-06-19 18:19:35 +0800

    chore(design-system): update how we handle import tailwindcss style to storybook (#500)
    Because
    
    - The storybook can't correctly import tailwind CSS
    
    This commit
    
    - update how we handle import tailwindcss style to storybook

--- packages/design-system/.storybook/main.ts
+++ packages/design-system/.storybook/main.ts
@@ -8,14 +8,6 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
-    {
-      name: "@storybook/addon-styling",
-      options: {
-        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
-        // For more details on this addon's options.
-        postCss: true,
-      },
-    },
   ],
   framework: {
     name: "@storybook/react-webpack5",
