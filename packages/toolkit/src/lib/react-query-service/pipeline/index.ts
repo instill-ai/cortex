b47b5232702762243279f8fcb4b985f8f863a816
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-06-27 13:06:17 +0800

    feat: add useRenamePipeline react-query hook

--- packages/toolkit/src/lib/react-query-service/pipeline/index.ts
+++ packages/toolkit/src/lib/react-query-service/pipeline/index.ts
@@ -3,6 +3,7 @@ export * from "./useCreatePipeline";
 export * from "./usePipeline";
 export * from "./usePipelines";
 export * from "./usePipelineSchema";
+export * from "./useRenamePipeline";
 export * from "./useUpdatePipeline";
 export * from "./useDeletePipeline";
 export * from "./useActivatePipeline";
