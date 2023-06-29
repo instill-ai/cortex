f7928046653e0a1002c4877b0ea73d837772c1b6
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-06-30 00:13:14 +0800

    feat: add another test item

--- packages/design-system/src/new-ui/Select/Select.stories.tsx
+++ packages/design-system/src/new-ui/Select/Select.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Select } from "./Select";
 import { Icons } from "../Icons";
+import { ModelLogo } from "../../ui";
 
 const meta: Meta<typeof Select> = {
   title: "Components/NewUi/Select",
@@ -38,18 +39,26 @@ export const Regular: Story = {
 
 export const WithIcon: Story = {
   render: () => (
-    <Select.Root>
+    <Select.Root open={true}>
       <Select.Trigger className="w-full">
         <Select.Value placeholder="Select a fruit" />
       </Select.Trigger>
       <Select.Content>
         <Select.Group>
           <Select.Label>Fruits</Select.Label>
-          <Select.Item value="apple">
+          <Select.Item value="Hello">
             <div className="flex flex-row space-x-2">
               <Icons.Box className="w-5 h-5 stroke-semantic-fg-primary group-hover:stroke-semantic-bg-primary" />
               <p className=" product-body-text-3-regular text-semantic-fg-primary group-hover:text-semantic-bg-primary">
                 Hello
+              </p>
+            </div>
+          </Select.Item>
+          <Select.Item value="YO">
+            <div className="flex flex-row space-x-2">
+              <ModelLogo variant="square" width={20} />
+              <p className=" product-body-text-3-regular text-semantic-fg-primary group-hover:text-semantic-bg-primary">
+                YO
               </p>
             </div>
           </Select.Item>
