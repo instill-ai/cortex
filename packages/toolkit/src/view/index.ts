fe474f4f912c46cc6c60db9adabc63d905341a1d
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-06-30 15:58:28 +0800

    feat: add CreateBlockchainForm

--- packages/toolkit/src/view/index.ts
+++ packages/toolkit/src/view/index.ts
@@ -1,5 +1,6 @@
-export * from "./airbyte";
 export * from "./ai";
+export * from "./airbyte";
+export * from "./blockchan";
 export * from "./destination";
 export * from "./mgmt";
 export * from "./model";
