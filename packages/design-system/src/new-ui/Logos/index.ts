85d7b7f040b24e081fd9671741133d467d053166
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-06-30 16:40:01 +0800

    feat: add Logos

--- packages/design-system/src/new-ui/Logos/index.ts
+++ packages/design-system/src/new-ui/Logos/index.ts
@@ -1,6 +1,15 @@
 import { MDLExpand } from "./MDLExpand";
+import { MDLSquare } from "./MDLSquare";
 import { Number } from "./Number";
+import { StabilityAI } from "./StabilityAI";
 import { VDPExpand } from "./VDPExpand";
 import { VDPSquare } from "./VDPSquare";
 
-export const Logos = { MDLExpand, Number, VDPExpand, VDPSquare };
+export const Logos = {
+  MDLExpand,
+  MDLSquare,
+  Number,
+  StabilityAI,
+  VDPExpand,
+  VDPSquare,
+};
