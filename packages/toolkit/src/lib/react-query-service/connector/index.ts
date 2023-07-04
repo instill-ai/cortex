dc60cf5c04c701e4b48d091c4410cea0f55fe2b7
Po-Chun Chiu <57251712+EiffelFly@users.noreply.github.com>
2023-07-04 14:31:08 +0800

    feat(toolkit): add dis/connect connector SDK (#556)
    Because
    
    - We need to connect/disconnect connector
    
    This commit
    
    - add dis/connect connector SDK

--- packages/toolkit/src/lib/react-query-service/connector/index.ts
+++ packages/toolkit/src/lib/react-query-service/connector/index.ts
@@ -1,3 +1,4 @@
+export * from "./useConnectConnector";
 export * from "./useConnector";
 export * from "./useConnectorDefinition";
 export * from "./useConnectorDefinitions";
@@ -6,6 +7,7 @@ export * from "./useConnectors";
 export * from "./useConnectorsWithPipelines";
 export * from "./useCreateConnector";
 export * from "./useDeleteConnector";
+export * from "./useDisonnectConnector";
 export * from "./useUpdateConnector";
 export * from "./useWatchConnector";
 export * from "./useWatchConnectors";
