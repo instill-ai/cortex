d92544112da2f87e0cb9b348ab43edda47883ee0
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-07-07 13:08:34 +0800

    fix: fix ai connector icons issue

--- packages/toolkit/src/view/ai/CreateAIForm.tsx
+++ packages/toolkit/src/view/ai/CreateAIForm.tsx
@@ -276,7 +276,7 @@ export const CreateAIForm = (props: CreateAIFormProps) => {
                       >
                         <div className="flex flex-row space-x-2">
                           <ImageWithFallback
-                            src={"/icons/stability-ai/logo.png"}
+                            src={"/icons/stabilityAI/stabilityai.svg"}
                             width={20}
                             height={20}
                             alt="Stability AI model logo"
