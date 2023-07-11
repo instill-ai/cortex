f18296d1a0376298c79f44bcc6fc5eecdf169422
Naman Anand <iamnamananand996@gmail.com>
2023-07-11 14:55:29 +0530

    feat: tooltip blink issue fix, arrow independent example

--- packages/design-system/src/new-ui/Tooltip/Tooltip.stories.tsx
+++ packages/design-system/src/new-ui/Tooltip/Tooltip.stories.tsx
@@ -30,6 +30,40 @@ export const Regular: Story = {
                 </div>
               </div>
             </div>
+            <Tooltip.Arrow
+              className="fill-white"
+              offset={10}
+              width={18}
+              height={12}
+            />
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    </Tooltip.Provider>
+  ),
+};
+
+export const TooltipWithoutArrow: Story = {
+  render: () => (
+    <Tooltip.Provider>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <button className="IconButton rounded p-2 shadow">Hover</button>
+        </Tooltip.Trigger>
+        <Tooltip.Portal>
+          <Tooltip.Content className="TooltipContent" sideOffset={5}>
+            <div className="Content inline-flex w-80 flex-col items-start justify-start bg-white p-3">
+              <div className="TextAndSupportingText flex flex-col items-start justify-start gap-1 self-stretch">
+                <div className="Text self-stretch text-sm font-semibold leading-none text-gray-800">
+                  Number of triggers tooltip
+                </div>
+                <div className="SupportingText self-stretch text-sm font-medium leading-none text-gray-800 text-opacity-80">
+                  Tooltips are used to describe or identify an element. In most
+                  scenarios, tooltips help the user understand meaning, function
+                  or alt-text.
+                </div>
+              </div>
+            </div>
           </Tooltip.Content>
         </Tooltip.Portal>
       </Tooltip.Root>
@@ -62,6 +96,12 @@ export const TooltipRight: Story = {
                 </div>
               </div>
             </div>
+            <Tooltip.Arrow
+              className="fill-white"
+              offset={10}
+              width={18}
+              height={12}
+            />
           </Tooltip.Content>
         </Tooltip.Portal>
       </Tooltip.Root>
@@ -81,7 +121,6 @@ export const TooltipLeft: Story = {
             className="TooltipContent"
             sideOffset={5}
             side={"left"}
-            avoidCollisions={true}
           >
             <div className="Content inline-flex w-80 flex-col items-start justify-start bg-white p-3">
               <div className="TextAndSupportingText flex flex-col items-start justify-start gap-1 self-stretch">
@@ -95,6 +134,12 @@ export const TooltipLeft: Story = {
                 </div>
               </div>
             </div>
+            <Tooltip.Arrow
+              className="fill-white"
+              offset={10}
+              width={18}
+              height={12}
+            />
           </Tooltip.Content>
         </Tooltip.Portal>
       </Tooltip.Root>
@@ -127,6 +172,12 @@ export const TooltipBottom: Story = {
                 </div>
               </div>
             </div>
+            <Tooltip.Arrow
+              className="fill-white"
+              offset={10}
+              width={18}
+              height={12}
+            />
           </Tooltip.Content>
         </Tooltip.Portal>
       </Tooltip.Root>
