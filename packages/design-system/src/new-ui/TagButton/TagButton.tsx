88ed98922121e39b85d63de2f93f339dc86032ee
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-07-13 14:53:49 +0800

    feat: update TagButton style and enable composition

--- packages/design-system/src/new-ui/TagButton/TagButton.tsx
+++ packages/design-system/src/new-ui/TagButton/TagButton.tsx
@@ -1,14 +1,15 @@
 import cn from "clsx";
 import React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
+import { Slot } from "@radix-ui/react-slot";
 
 const tagButtonVariants = cva(
-  "inline-flex items-center rounded-full font-semibold transition-colors",
+  "inline-flex items-center rounded-full transition-colors",
   {
     variants: {
       variant: {
         default:
-          "text-semantic-fg-primary hover:border hover:border-semantic-bg-line focus:border-semantic-bg-line dark:hover:border hover:border-sementic-bg-secondary-alt-primary dark:text-sementic-fg-on-default dark:hover:bg-sementic-bg-secondary-secondary",
+          "text-semantic-fg-primary border-opacity-0 border border-[#E1E6EF] hover:border-opacity-100 focus:border-opacity-100 hover:bg-semantic-bg-base-bg focus:bg-semantic-bg-secondary",
       },
       size: {
         sm: "px-2 py-1 product-body-text-4-medium",
@@ -23,14 +24,16 @@ const tagButtonVariants = cva(
   }
 );
 
-export interface TagButtonProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof tagButtonVariants> {}
+export type TagButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof tagButtonVariants> & {
+    asChild?: boolean;
+  };
 
-const TagButton = React.forwardRef<HTMLDivElement, TagButtonProps>(
-  ({ className, variant, size, ...props }, ref) => {
+const TagButton = React.forwardRef<HTMLButtonElement, TagButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
     return (
-      <div
+      <Comp
         className={cn(tagButtonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
