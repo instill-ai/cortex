bc6fc69336bff6a45b9bd21b5810ef32b54dd023
Naman Anand <iamnamananand996@gmail.com>
2023-07-14 10:41:34 +0530

    feat: example for asChild in components (#640)
    Because
    
    - we need to example for asChild in components in stories
    
    This commit
    
    - example for asChild in components

--- packages/design-system/src/new-ui/TagButton/TagButton.stories.tsx
+++ packages/design-system/src/new-ui/TagButton/TagButton.stories.tsx
@@ -12,3 +12,11 @@ type Story = StoryObj<typeof TagButton>;
 export const Dafault: Story = {
   render: () => <TagButton>Label</TagButton>,
 };
+
+export const PrimaryWithAsChild: Story = {
+  render: () => (
+    <TagButton className="gap-x-2" variant="default" size="lg" asChild>
+      <a href="https://www.instill.tech/">Instill AI</a>
+    </TagButton>
+  ),
+};
