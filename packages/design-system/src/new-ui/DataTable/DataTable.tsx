7592e61b29a5256a5a062bb39072e888337ee25d
Naman Anand <iamnamananand996@gmail.com>
2023-07-18 21:53:38 +0530

    feat: Namananand/ins 1312 datatable component (#654)
    Because
    
    - we need a `<Datatable />` component
    
    This commit
    
    - `<Datatable />` component

---
+++ packages/design-system/src/new-ui/DataTable/DataTable.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { Table } from "../Table";
+import * as React from "react";
+import { DataTablePagination } from "./DataTablePagination";
+import { Input } from "../Input";
+import { Nullable } from "../../types/general";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+  pageSize: number;
+  searchPlaceholder: Nullable<string>;
+  searchKey: Nullable<string>;
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+  pageSize,
+  searchPlaceholder,
+  searchKey,
+}: DataTableProps<TData, TValue>) {
+  const [rowSelection, setRowSelection] = React.useState({});
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onRowSelectionChange: setRowSelection,
+    initialState: {
+      pagination: {
+        pageSize: pageSize,
+      },
+    },
+    state: {
+      rowSelection,
+      sorting,
+      columnFilters,
+    },
+  });
+
+  return (
+    <div>
+      <div className="flex items-center py-4">
+        {searchKey && (
+          <Input.Root className="w-1/4">
+            <Input.Core
+              type="text"
+              placeholder={searchPlaceholder || ""}
+              value={
+                (table.getColumn(searchKey)?.getFilterValue() as string) ?? ""
+              }
+              onChange={(event) =>
+                table.getColumn(searchKey)?.setFilterValue(event.target.value)
+              }
+            />
+          </Input.Root>
+        )}
+      </div>
+      <Table.Root>
+        <Table.Header>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <Table.Row key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <Table.Head key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </Table.Head>
+                );
+              })}
+            </Table.Row>
+          ))}
+        </Table.Header>
+        <Table.Body>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <Table.Row
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <Table.Cell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </Table.Cell>
+                ))}
+              </Table.Row>
+            ))
+          ) : (
+            <Table.Row>
+              <Table.Cell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </Table.Cell>
+            </Table.Row>
+          )}
+        </Table.Body>
+      </Table.Root>
+
+      <DataTablePagination table={table} />
+    </div>
+  );
+}
---
+++ packages/design-system/src/new-ui/DataTable/DataTablePagination.tsx
@@ -0,0 +1,52 @@
+import { Table } from "@tanstack/react-table";
+import { Button } from "../Button";
+import { Icons } from "../Icons";
+import cn from "clsx";
+
+interface DataTablePaginationProps<TData> {
+  table: Table<TData>;
+}
+
+export function DataTablePagination<TData>({
+  table,
+}: DataTablePaginationProps<TData>) {
+  return (
+    <div className="flex items-center justify-end py-8">
+      <Button
+        className="gap-x-2 !rounded-r-none rounded-l-sm border-semantic-bg-line !py-2.5 px-4 hover:border-semantic-bg-line"
+        variant="secondaryGrey"
+        size="sm"
+        onClick={() => table.previousPage()}
+        disabled={!table.getCanPreviousPage()}
+      >
+        <Icons.ArrowNarrowLeft className="h-5 w-5 stroke-semantic-fg-secondary" />
+        <span className="product-body-text-3-semibold">Previous</span>
+      </Button>
+      {table.getPageOptions().map((e, index) => (
+        <Button
+          className={cn(
+            "!rounded-none border-l-0 border-semantic-bg-line !py-2.5 px-2.5 hover:border-semantic-bg-line",
+            table.getPageCount() === index + 1 && "border-r-0"
+          )}
+          variant="secondaryGrey"
+          size="sm"
+          onClick={() => table.setPageIndex(index)}
+          key={`table-page-button-${index}`}
+          disabled={index === table.options.state.pagination?.pageIndex}
+        >
+          <span className="px-2 product-body-text-3-semibold">{index + 1}</span>
+        </Button>
+      ))}
+      <Button
+        className="gap-x-2 !rounded-l-none rounded-r-sm border-semantic-bg-line !py-2.5 px-4 hover:border-semantic-bg-line"
+        variant="secondaryGrey"
+        size="sm"
+        onClick={() => table.nextPage()}
+        disabled={!table.getCanNextPage()}
+      >
+        <span className="product-body-text-3-semibold">Next</span>
+        <Icons.ArrowNarrowRight className="h-5 w-5 stroke-semantic-fg-secondary" />
+      </Button>
+    </div>
+  );
+}
