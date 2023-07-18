113876354754eabc3c141d096b51bdb501f842fb
Naman Anand <iamnamananand996@gmail.com>
2023-07-18 21:48:20 +0530

    feat: example update

--- packages/design-system/src/new-ui/DataTable/DataTable.stories.tsx
+++ packages/design-system/src/new-ui/DataTable/DataTable.stories.tsx
@@ -37,85 +37,85 @@ const data: PipelineTriggerCount[] = [
     pipeline_errored: 12,
   },
   {
-    pipeline_id: "Pipeline-name-2",
+    pipeline_id: "outrageous-indigo-ladybug",
     watchState: "STATE_ERROR",
-    pipeline_completed: 120,
+    pipeline_completed: 1320,
     pipeline_errored: 132,
   },
   {
-    pipeline_id: "Pipeline-name-3",
+    pipeline_id: "calm-coral-sloth",
     watchState: "STATE_ERROR",
     pipeline_completed: 923,
     pipeline_errored: 12,
   },
   {
-    pipeline_id: "Pipeline-name-1",
+    pipeline_id: "certain-aqua-flamingo",
     watchState: "STATE_ACTIVE",
-    pipeline_completed: 20,
-    pipeline_errored: 12,
+    pipeline_completed: 200,
+    pipeline_errored: 132,
   },
   {
-    pipeline_id: "Pipeline-name-2",
+    pipeline_id: "marked-aqua-amphibian",
     watchState: "STATE_ERROR",
     pipeline_completed: 220,
     pipeline_errored: 12,
   },
   {
-    pipeline_id: "Pipeline-name-3",
+    pipeline_id: "potential-rose-heron",
     watchState: "STATE_ACTIVE",
-    pipeline_completed: 20,
-    pipeline_errored: 12,
+    pipeline_completed: 230,
+    pipeline_errored: 112,
   },
   {
-    pipeline_id: "Pipeline-name-1",
+    pipeline_id: "calm-azure-goose",
     watchState: "STATE_ACTIVE",
     pipeline_completed: 320,
-    pipeline_errored: 12,
+    pipeline_errored: 142,
   },
   {
-    pipeline_id: "Pipeline-name-2",
+    pipeline_id: "fluttering-red-booby",
     watchState: "STATE_ERROR",
-    pipeline_completed: 20,
+    pipeline_completed: 720,
     pipeline_errored: 12,
   },
   {
-    pipeline_id: "Pipeline-name-3",
-    watchState: "STATE_ACTIVE",
-    pipeline_completed: 20,
-    pipeline_errored: 12,
+    pipeline_id: "efficient-apricot-ptarmigan",
+    watchState: "STATE_UNSPECIFIED",
+    pipeline_completed: 520,
+    pipeline_errored: 612,
   },
   {
-    pipeline_id: "Pipeline-name-1",
+    pipeline_id: "resonant-rose-booby",
     watchState: "STATE_ACTIVE",
     pipeline_completed: 20,
-    pipeline_errored: 2,
+    pipeline_errored: 82,
   },
   {
-    pipeline_id: "Pipeline-name-2",
+    pipeline_id: "competent-fuchsia-turkey",
     watchState: "STATE_ERROR",
     pipeline_completed: 270,
     pipeline_errored: 12,
   },
   {
-    pipeline_id: "Pipeline-name-3",
-    watchState: "STATE_ACTIVE",
-    pipeline_completed: 20,
+    pipeline_id: "robust-brown-ape",
+    watchState: "STATE_UNSPECIFIED",
+    pipeline_completed: 1120,
     pipeline_errored: 12,
   },
   {
-    pipeline_id: "Pipeline-name-1",
+    pipeline_id: "resonant-rose-booby",
     watchState: "STATE_ACTIVE",
     pipeline_completed: 210,
-    pipeline_errored: 12,
+    pipeline_errored: 312,
   },
   {
-    pipeline_id: "Pipeline-name-2",
+    pipeline_id: "Pipeline-name-14",
     watchState: "STATE_ERROR",
     pipeline_completed: 20,
     pipeline_errored: 12,
   },
   {
-    pipeline_id: "Pipeline-name-3",
+    pipeline_id: "competent-fuchsia-turkey",
     watchState: "STATE_ERROR",
     pipeline_completed: 260,
     pipeline_errored: 32,
@@ -129,50 +129,78 @@ export type PipelineTriggerCount = {
   watchState: PipelineState;
 };
 
-const columns: ColumnDef<PipelineTriggerCount>[] = [
-  {
-    accessorKey: "pipeline_id",
-    header: () => <div className="w-auto text-left">Pipeline Id</div>,
-    cell: ({ row }) => {
-      return (
-        <div className="flex min-w-[600px] flex-row gap-x-2">
-          <Checkbox
-            checked={row.getIsSelected()}
-            onCheckedChange={(value) => row.toggleSelected(!!value)}
-            aria-label="Select row"
-            className="h-5 w-5"
-          />
-          <div className="w-auto">{row.getValue("pipeline_id")}</div>
-        </div>
-      );
+export const Default = () => {
+  const columns: ColumnDef<PipelineTriggerCount>[] = [
+    {
+      accessorKey: "pipeline_id",
+      header: () => <div className="w-auto text-left">Pipeline Id</div>,
+      cell: ({ row }) => {
+        return (
+          <div className="flex min-w-[600px] flex-row gap-x-2">
+            <Checkbox
+              checked={row.getIsSelected()}
+              onCheckedChange={(value) => row.toggleSelected(!!value)}
+              aria-label="Select row"
+              className="h-5 w-5"
+            />
+            <div className="w-auto">{row.getValue("pipeline_id")}</div>
+          </div>
+        );
+      },
     },
-  },
-  {
-    accessorKey: "watchState",
-    accessorFn: (row) => row.watchState,
-    header: () => <div className="max-w-[80px] text-center">Status</div>,
-    cell: ({ row }) => {
-      return (
-        <div className="text-center">
-          <Tag
-            variant={
-              row.getValue("watchState") === "STATE_ACTIVE"
-                ? "lightGreen"
-                : "lightRed"
-            }
-            className="border-0"
-            size={"sm"}
-          >
-            Active
-          </Tag>
-        </div>
-      );
+    {
+      accessorKey: "watchState",
+      accessorFn: (row) => row.watchState,
+      header: () => <div className="max-w-[80px] text-center">Status</div>,
+      cell: ({ row }) => {
+        return (
+          <div className="text-center">
+            <Tag
+              variant={
+                row.getValue("watchState") === "STATE_ACTIVE"
+                  ? "lightGreen"
+                  : "lightRed"
+              }
+              className="border-0"
+              size={"sm"}
+            >
+              Active
+            </Tag>
+          </div>
+        );
+      },
     },
-  },
-  {
-    accessorKey: "pipeline_completed",
-    header: ({ column }) => {
-      return (
+    {
+      accessorKey: "pipeline_completed",
+      header: ({ column }) => {
+        return (
+          <div className="text-center">
+            <Button
+              className="gap-x-2 py-0"
+              variant="tertiaryGrey"
+              size="sm"
+              onClick={() =>
+                column.toggleSorting(column.getIsSorted() === "asc")
+              }
+            >
+              <span className="min-w-[130px]">Completed Triggers</span>
+              {getIcon(column.getIsSorted())}
+            </Button>
+          </div>
+        );
+      },
+
+      cell: ({ row }) => {
+        return (
+          <div className="text-center text-semantic-fg-secondary">
+            {row.getValue("pipeline_completed")}
+          </div>
+        );
+      },
+    },
+    {
+      accessorKey: "pipeline_errored",
+      header: ({ column }) => (
         <div className="text-center">
           <Button
             className="gap-x-2 py-0"
@@ -180,48 +208,22 @@ const columns: ColumnDef<PipelineTriggerCount>[] = [
             size="sm"
             onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
           >
-            <span className="min-w-[130px]">Completed Triggers</span>
+            <span className="min-w-[110px]">Errored Triggers</span>
             {getIcon(column.getIsSorted())}
           </Button>
         </div>
-      );
+      ),
+      cell: ({ row }) => {
+        return (
+          <div className="text-center text-semantic-fg-secondary">
+            {row.getValue("pipeline_errored")}
+          </div>
+        );
+      },
     },
+  ];
 
-    cell: ({ row }) => {
-      return (
-        <div className="text-center text-semantic-fg-secondary">
-          {row.getValue("pipeline_completed")}
-        </div>
-      );
-    },
-  },
-  {
-    accessorKey: "pipeline_errored",
-    header: ({ column }) => (
-      <div className="text-center">
-        <Button
-          className="gap-x-2 py-0"
-          variant="tertiaryGrey"
-          size="sm"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          <span className="min-w-[110px]">Errored Triggers</span>
-          {getIcon(column.getIsSorted())}
-        </Button>
-      </div>
-    ),
-    cell: ({ row }) => {
-      return (
-        <div className="text-center text-semantic-fg-secondary">
-          {row.getValue("pipeline_errored")}
-        </div>
-      );
-    },
-  },
-];
-
-export const Default: Story = {
-  render: () => (
+  return (
     <DataTable
       columns={columns}
       data={data}
@@ -229,5 +231,5 @@ export const Default: Story = {
       searchPlaceholder="Search Pipeline"
       searchKey="pipeline_id"
     />
-  ),
+  );
 };
