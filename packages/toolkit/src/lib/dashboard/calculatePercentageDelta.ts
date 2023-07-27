2d035462c61e96c7b4fb57a89fcbef3d88a9eaae
Naman Anand <iamnamananand996@gmail.com>
2023-07-27 21:25:18 +0530

    fix: delta changes calculation

--- packages/toolkit/src/lib/dashboard/calculatePercentageDelta.ts
+++ packages/toolkit/src/lib/dashboard/calculatePercentageDelta.ts
@@ -6,7 +6,10 @@ export function calculatePercentageDelta(
     return 0; // Both counts are zero, return 0 as percentage change
   }
   if (previousCount === 0) {
-    return currentCount; // Previous count is zero, change is currentCount
+    return currentCount * 100; // Previous count is zero, change is currentCount * 100
+  }
+  if (currentCount === 0) {
+    return previousCount * -100; // Current count is zero, change is previousCount * -100
   }
   const delta = currentCount - previousCount;
   const percentageDelta = (delta / previousCount) * 100;
