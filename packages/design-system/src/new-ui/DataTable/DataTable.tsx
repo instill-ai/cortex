58add360d9352dbf02a4bfbffc9e92c523bc1c69
Naman Anand <iamnamananand996@gmail.com>
2023-08-08 23:37:11 +0530

    chore: cancel button for table search

--- packages/design-system/src/new-ui/DataTable/DataTable.tsx
+++ packages/design-system/src/new-ui/DataTable/DataTable.tsx
@@ -102,6 +102,13 @@ const DataTable = <TData, TValue>({
                   table.getColumn(searchKey)?.setFilterValue(event.target.value)
                 }
               />
+              <Input.LeftIcon
+                onClick={() => {
+                  table.getColumn(searchKey)?.setFilterValue("");
+                }}
+              >
+                <Icons.X className="my-auto h-5 w-5 cursor-pointer stroke-semantic-fg-secondary" />
+              </Input.LeftIcon>
             </Input.Root>
           )}
         </div>
