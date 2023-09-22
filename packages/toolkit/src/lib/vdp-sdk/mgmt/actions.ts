d1a1b02ad96e1423ea1dbfab18591c07cd82d758
Po-Chun Chiu <57251712+EiffelFly@users.noreply.github.com>
2023-09-22 21:44:22 +0800

    feat(toolkit): add auth related endpoint (#878)
    Because
    
    - We are going to support simple auth in instill-core
    
    This commit
    
    - add auth related endpoint

---
+++ packages/toolkit/src/lib/vdp-sdk/mgmt/actions.ts
@@ -0,0 +1,37 @@
+import { Nullable } from "../../type";
+import { createInstillAxiosClient } from "../helper";
+
+export async function authLogoutAction({
+  accessToken,
+}: {
+  accessToken: Nullable<string>;
+}) {
+  try {
+    const client = createInstillAxiosClient(accessToken, "base");
+
+    await client.post("/auth/logout");
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+export type AuthLoginActionPayload = {
+  username: string;
+  password: string;
+};
+
+export async function authLoginAction({
+  payload,
+  accessToken,
+}: {
+  payload: AuthLoginActionPayload;
+  accessToken: Nullable<string>;
+}) {
+  try {
+    const client = createInstillAxiosClient(accessToken, "base");
+
+    await client.post("/auth/login", payload);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
