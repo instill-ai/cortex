837e268b128e626a53d530468caebe0ad7c6949a
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-09-27 17:06:41 +0800

    feat: add further logic

--- packages/toolkit/src/view/pipeline-builder/components/SharePipelineDialog.tsx
+++ packages/toolkit/src/view/pipeline-builder/components/SharePipelineDialog.tsx
@@ -147,8 +147,93 @@ export const SharePipelineDialog = (props: SharePipelineDialogProps) => {
               disabled={!pipeline.isSuccess}
             />
           </div>
+          {!pipelineIsPublic ? (
+            <div className="mb-3 flex flex-col px-6 py-3">
+              <div className="flex flex-row gap-x-2">
+                <p className="product-body-text-3-semibold">Share by link</p>
+                <Tooltip.Provider>
+                  <Tooltip.Root>
+                    <Tooltip.Trigger asChild={true}>
+                      <Icons.HelpCircle className="w-4 h-4 stroke-semantic-fg-secondary" />
+                    </Tooltip.Trigger>
+                    <Tooltip.Portal>
+                      <Tooltip.Content className="!px-3 !py-2 rounded-sm !product-body-text-4-semibold bg-semantic-bg-primary">
+                        Users with the link can view the pipeline, but they can
+                        not edit or trigger the pipeline
+                        <Tooltip.Arrow
+                          className="fill-semantic-bg-primary"
+                          offset={10}
+                          width={18}
+                          height={12}
+                        />
+                      </Tooltip.Content>
+                    </Tooltip.Portal>
+                  </Tooltip.Root>
+                </Tooltip.Provider>
+              </div>
+              <Switch
+                checked={false}
+                onCheckedChange={async (check) => {
+                  if (!pipeline.isSuccess) return;
+
+                  // const payload: UpdateUserPipelinePayload = {
+                  //   name: `users/${entity}/pipelines/${id}`,
+                  //   permission: {
+                  //     users: {
+                  //       ...pipeline.data.permission.users,
+                  //       "users/*": {
+                  //         enabled: check,
+                  //         role: "ROLE_VIEWER",
+                  //       },
+                  //     },
+                  //     share_code: {
+                  //       ...pipeline.data?.permission.share_code,
+                  //     },
+                  //   },
+                  // };
+
+                  // try {
+                  //   updatePipeline.mutateAsync({
+                  //     payload,
+                  //     accessToken,
+                  //   });
+                  // } catch (error) {
+                  //   if (isAxiosError(error)) {
+                  //     toast({
+                  //       title:
+                  //         "Something went wrong when switch pipeline permission",
+                  //       variant: "alert-error",
+                  //       size: "large",
+                  //       description: getInstillApiErrorMessage(error),
+                  //     });
+                  //   } else {
+                  //     toast({
+                  //       title:
+                  //         "Something went wrong when switch pipeline permission",
+                  //       variant: "alert-error",
+                  //       size: "large",
+                  //       description: "Please try again later",
+                  //     });
+                  //   }
+                  // }
+                }}
+                disabled={!pipeline.isSuccess}
+              />
+            </div>
+          ) : null}
           <div className="flex flex-row justify-between">
-            <div className="flex flex-row"></div>
+            <div className="flex flex-row gap-x-3">
+              {pipelineIsPublic ? (
+                <Icons.LockUnlocked03 className="w-6 h-6 stroke-semantic-fg-secondary" />
+              ) : (
+                <Icons.Lock03 className="w-6 h-6 stroke-semantic-fg-secondary" />
+              )}
+              <p className="product-button-button-3">
+                {pipelineIsPublic
+                  ? "Anyone can view this pipeline"
+                  : "Anyone with the link"}
+              </p>
+            </div>
           </div>
         </div>
         <Dialog.Close />
